fe3ec95e8ec7522e85c5e56a609dd034b1f3a77f
Katie Davis <katie.davis@shopify.com>
2019-05-29 10:49:35 -0400

    add tests for `generate webhook`

--- test/commands/generate/webhook_test.rb
+++ test/commands/generate/webhook_test.rb
@@ -9,16 +9,25 @@ module ShopifyCli
         def setup
           super
           @command = ShopifyCli::Commands::Generate.new(@context)
+          ShopifyCli::Tasks::GetSchema.expects(:call).returns(
+            JSON.parse(File.read(File.join(ShopifyCli::ROOT, "test/fixtures/shopify_schema.json")))
+          )
+          ShopifyCli::Project.expects(:current).returns(
+            TestHelpers::FakeProject.new(
+              directory: @context.root,
+              config: {
+                'app_type' => 'node',
+              }
+            )
+          )
         end
 
         def test_with_param
-          ShopifyCli::Project.write(@context, :node)
           @context.expects(:exec).with('npm run-script generate-webhook PRODUCT_CREATE')
           @command.call(['webhook', 'PRODUCT_CREATE'], nil)
         end
 
         def test_with_selection
-          ShopifyCli::Project.write(@context, :node)
           CLI::UI::Prompt.expects(:ask).returns('PRODUCT_CREATE')
           @context.expects(:exec).with('npm run-script generate-webhook PRODUCT_CREATE')
           @command.call(['webhook'], nil)
