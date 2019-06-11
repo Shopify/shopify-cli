eea54749d71f977f7f13011d3740a36a7c7cec5e
Katie Davis <katie.davis@shopify.com>
2019-06-11 15:32:43 -0400

    Show error if clone fails

--- lib/shopify-cli/tasks/clone.rb
+++ lib/shopify-cli/tasks/clone.rb
@@ -13,12 +13,18 @@ module ShopifyCli
 
       def git_progress(*git_command)
         CLI::UI::Progress.progress do |bar|
+          msg = []
           success = CLI::Kit::System.system('git', *git_command, '--progress') do |_out, err|
-            next unless err.strip.start_with?('Receiving objects:')
-            percent = (err.match(/Receiving objects:\s+(\d+)/)[1].to_f / 100).round(2)
-            bar.tick(set_percent: percent)
+            if err.strip.start_with?('Receiving objects:')
+              percent = (err.match(/Receiving objects:\s+(\d+)/)[1].to_f / 100).round(2)
+              bar.tick(set_percent: percent)
+              next
+            end
+            msg << err
           end.success?
-          return false unless success
+          unless success
+            raise CLI::UI.fmt("{{red:#{msg.join("\n")}}}")
+          end
           bar.tick(set_percent: 1.0)
           true
         end
