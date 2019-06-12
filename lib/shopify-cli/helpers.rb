0e624b085ea9fd4388e130a735d41d6e0b6c5e00
Tyler Ball <tyler@tylerball.net>
2019-06-12 11:14:27 -0400

    organize and alphabetize autoloads

---
+++ lib/shopify-cli/helpers.rb
@@ -0,0 +1,11 @@
+module ShopifyCli
+  module Helpers
+    autoload :EnvFile, 'shopify-cli/helpers/env_file'
+    autoload :Gem, 'shopify-cli/helpers/gem'
+    autoload :GraphQL, 'shopify-cli/helpers/graphql'
+    autoload :PidFile, 'shopify-cli/helpers/pid_file'
+    autoload :ProcessSupervision, 'shopify-cli/helpers/process_supervision'
+    autoload :SchemaParser, 'shopify-cli/helpers/schema_parser'
+    autoload :ShopifySchema, 'shopify-cli/helpers/shopify_schema'
+  end
+end
