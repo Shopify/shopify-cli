45ecdf7be9addc226443c6a252786fd157ececab
Dave Newton <dave.newton@shopify.com>
2019-06-18 17:09:15 -0400

    fix rails create

--- lib/shopify-cli/app_types/rails.rb
+++ lib/shopify-cli/app_types/rails.rb
@@ -13,6 +13,7 @@ module ShopifyCli
             SHOPIFY_API_SECRET_KEY={secret}
             HOST={host}
             SHOP={shop}
+            SCOPES={scopes}
           KEYS
         end
 
@@ -44,20 +45,46 @@ module ShopifyCli
         CLI::UI::Frame.open("Generating new rails app project in #{name}...") do
           ctx.system(Gem.binary_path_for(ctx, 'rails'), 'new', name)
         end
-        CLI::UI::Frame.open("Adding shopify_app gem...") do
-          ctx.system('echo', '"gem \'shopify_app\'"', '>>', 'Gemfile')
+
+        File.open(File.join(ctx.root, 'Gemfile'), 'a') do |f|
+          f.puts "\ngem 'shopify_app'"
+        end
+        ctx.puts("{{green:✔︎}} Adding shopify_app gem…")
+        CLI::UI::Frame.open("Installing bundler…") do
+          ctx.system('gem', 'install', 'bundler', '-v', '~>1.0', chdir: ctx.root)
+          ctx.system('gem', 'install', 'bundler', '-v', '~>2.0', chdir: ctx.root)
         end
         CLI::UI::Frame.open("Running bundle install...") do
           ctx.system(Gem.binary_path_for(ctx, 'bundle'), 'install', chdir: ctx.root)
         end
         CLI::UI::Frame.open("Running shopfiy_app generator...") do
+          begin
+            ctx.system(Gem.binary_path_for(ctx, 'spring'), 'stop', chdir: ctx.root)
+          rescue
+            # no op
+          end
           ctx.system(
             Gem.binary_path_for(ctx, 'rails'),
             'generate',
-            'shopify_app', "--api_key #{ctx.app_metadata[:api_key]}", "--secret #{ctx.app_metadata[:secret]}"
+            'shopify_app', "--api_key #{ctx.app_metadata[:api_key]}", "--secret #{ctx.app_metadata[:secret]}",
+            chdir: ctx.root
           )
         end
+        CLI::UI::Frame.open('Running migrations…') do
+          ctx.system(Gem.binary_path_for(ctx, 'rails'), 'db:migrate', 'RAILS_ENV=development', chdir: ctx.root)
+        end
         ShopifyCli::Finalize.request_cd(name)
+
+        env_file = Helpers::EnvFile.new(
+          app_type: self,
+          api_key: ctx.app_metadata[:api_key],
+          secret: ctx.app_metadata[:secret],
+          host: ctx.app_metadata[:host],
+          shop: ctx.app_metadata[:shop],
+          scopes: 'write_products,write_customers,write_orders',
+        )
+        env_file.write(ctx, '.env')
+
         puts CLI::UI.fmt(post_clone)
       end
 
