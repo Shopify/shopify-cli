3ca148398e96b05f50e6227ecded630561dc3e77
Tyler Ball <tyler@tylerball.net>
2019-06-20 13:55:45 -0400

    pass GEM_HOME to rails server (#183)

--- test/shopify-cli/helpers/gem_test.rb
+++ test/shopify-cli/helpers/gem_test.rb
@@ -6,31 +6,38 @@ module ShopifyCli
     class GemTest < MiniTest::Test
       include TestHelpers::Context
 
+      def setup
+        super
+        @home = Dir.mktmpdir
+        @context.setenv('HOME', @home)
+      end
+
+      def teardown
+        @context.setenv('GEM_HOME', nil)
+      end
+
       def test_install_installs_with_gem_home_unpopulated
-        @context.expects(:mkdir_p).with(
-          "#{@context.getenv('HOME')}/.gem/ruby/#{RUBY_VERSION}"
-        ).at_least_once
-        @context.expects(:setenv).with(
-          'GEM_HOME',
-          "#{@context.getenv('HOME')}/.gem/ruby/#{RUBY_VERSION}"
-        ).at_least_once
         @context.expects(:system).with('gem install mygem')
         Gem.install(@context, 'mygem')
       end
 
       def test_install_installs_with_gem_home_populated
-        @context.setenv('GEM_HOME', '~/.gem/ruby/2.5.5')
+        @context.setenv('GEM_HOME', "#{@home}/.gem/ruby/#{RUBY_VERSION}")
         @context.expects(:system).with('gem install mygem')
         Gem.install(@context, 'mygem')
       end
 
       def test_install_does_not_install_if_installed
-        @context.setenv('GEM_HOME', "~/.gem/ruby/#{RUBY_VERSION}")
-        Dir.expects(:glob).with("~/.gem/ruby/#{RUBY_VERSION}/gems/mygem-*").returns([
-          "~/.gem/ruby/#{RUBY_VERSION}/gems/mygem-1.0.0",
+        @context.setenv('GEM_HOME', "#{@home}/.gem/ruby/#{RUBY_VERSION}")
+        Dir.expects(:glob).with("#{@home}/.gem/ruby/#{RUBY_VERSION}/gems/mygem-*").returns([
+          "#{@home}/.gem/ruby/#{RUBY_VERSION}/gems/mygem-1.0.0",
         ]).at_least_once
         @context.expects(:system).with('gem install mygem').never
         Gem.install(@context, 'mygem')
+      end
+
+      def test_gem_home_returns_proper_path
+        assert_equal("#{@home}/.gem/ruby/#{RUBY_VERSION}", Gem.gem_home(@context))
       end
     end
   end
