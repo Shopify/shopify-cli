e75989cfeaef1a3679e40ff9688b002ca936b9b3
Tim Anema <tim.anema@shopify.com>
2019-09-16 11:20:24 -0400

    fixing store use in schema

--- lib/shopify-cli/tasks/schema.rb
+++ lib/shopify-cli/tasks/schema.rb
@@ -5,13 +5,11 @@ module ShopifyCli
     class Schema < ShopifyCli::Task
       include ShopifyCli::Helpers::GraphQL::Queries
 
-      SCHEMA_KEY = "shopify_admin_schema"
-
       def call(ctx)
-        unless Helpers::Store.exists?(SCHEMA_KEY)
-          Helpers::Store.set(SCHEMA_KEY, JSON.dump(Helpers::AdminAPI.query(ctx, introspection)))
+        unless Helpers::Store.exists?(:shopify_admin_schema)
+          Helpers::Store.set(shopify_admin_schema: JSON.dump(Helpers::AdminAPI.query(ctx, introspection)))
         end
-        schema = JSON.parse(Helpers::Store.get(SCHEMA_KEY))
+        schema = JSON.parse(Helpers::Store.get(:shopify_admin_schema))
         ctx.app_metadata = { schema: schema }
         schema
       end
