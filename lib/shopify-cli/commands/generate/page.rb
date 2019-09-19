3c74dedc34bb862848a8675ca148f86be0fcd4d7
Katie Davis <katie.davis@shopify.com>
2019-09-19 16:31:59 -0400

    add the ability to read type

--- lib/shopify-cli/commands/generate/page.rb
+++ lib/shopify-cli/commands/generate/page.rb
@@ -4,28 +4,38 @@ module ShopifyCli
   module Commands
     class Generate
       class Page < ShopifyCli::SubCommand
+        options do |parser, flags|
+          parser.on('--type=TYPE') do |t|
+            flags[:type] = t
+          end
+        end
+
         def call(args, _name)
-          unless args[1]
+          project = ShopifyCli::Project.current
+          types = project.app_type.page_types
+          name = args[1]
+          flag = options.flags[:type]
+          selected_type = types[flag.downcase]
+          if !name || name.include?('--')
             @ctx.puts(self.class.help)
             return
           end
-          project = ShopifyCli::Project.current
-          name = args[1]
-          types = project.app_type.page_types
           # temporary check until we build for rails
           if project.app_type == ShopifyCli::AppTypes::Rails
             raise(ShopifyCli::Abort, 'This feature is not yet available for Rails apps')
           end
 
-          selected_type = CLI::UI::Prompt.ask('Which template would you like to use?') do |handler|
-            types.each do |key, value|
-              handler.option(key) { value }
+          unless selected_type
+            selected_type = CLI::UI::Prompt.ask('Which template would you like to use?') do |handler|
+              types.each do |key, value|
+                handler.option(key) { value }
+              end
             end
           end
           spin_group = CLI::UI::SpinGroup.new
           spin_group.add("Generating #{types.key(selected_type)} page...") do |spinner|
             ShopifyCli::Commands::Generate.run_generate(
-              "#{project.app_type.generate[selected_type]} #{name}", name, @ctx
+              "#{project.app_type.generate[selected_type]} #{name}", name
             )
             spinner.update_title("{{green: #{name}}} generated in /pages/#{name}")
           end
