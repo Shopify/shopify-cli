fde67a95232072924a85d6d12552f4608aaaadc8
Katie Davis <katie.davis@shopify.com>
2019-10-22 14:58:48 -0400

    add graphql files

--- lib/shopify-cli/tasks/update_whitelist_url.rb
+++ lib/shopify-cli/tasks/update_whitelist_url.rb
@@ -4,45 +4,25 @@ module ShopifyCli
 
       def call(ctx, url: url)
         @ctx = ctx
-        @app = ShopifyCli::Tasks::GetApp.call(@ctx)
-        @url = ShopifyCli::Tasks::Tunnel.call(@ctx)
-        puts @app
+        api_key = Project.current.env.api_key
+        result = Helpers::PartnersAPI.query(ctx, 'get_app_urls', apiKey: api_key)
+        app = result['data']['app']
+        whitelist_urls = check_urls(app['redirectUrlWhitelist'], url)
+        return if whitelist_urls == app['redirectUrlWhitelist']
+        puts ShopifyCli::Helpers::PartnersAPI.query(@ctx, 'update_whitelisturls', input: {
+          redirectUrlWhitelist: whitelist_urls, apiKey: api_key
+        })
       end
 
-      def check_urls
-        whitelist_input = @app['redirectUrlWhitelist'].map do |url|
+      def check_urls(urls, new_url)
+        urls.map do |url|
           # 'https://d1ea5a3e.ngrok.io' only a-f0-9
           if match = url.match(/https:\/\/([a-z0-9\-]+\.ngrok\.io)(.*)/)
-            "https://#{new_url}#{match[2]}"
+            "#{new_url}#{match[2]}"
           else
             url
           end
         end
-      end
-
-      def mutation(urls)
-        <<~QUERY
-            mutation appUpdate($input: AppCreateInput!) {
-              appUpdate(input: $input) {
-                app {
-                  apiKey
-                  apiSecretKeys {
-                    secret
-                    createdAt
-                  }
-                  #{urls}
-                }
-                userErrors {
-                  message
-                  field
-                }
-              }
-            }
-        QUERY
-      end
-
-      def perform_mutation
-        ShopifyCli::Helpers::PartnersAPI.query(@ctx, mutation, input: {redirectUrlWhitelist: [@url]})
       end
     end
   end
