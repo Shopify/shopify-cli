e6039748e7cdf4c732cc61527832a02f5c981777
Katie Davis <katie.davis@shopify.com>
2019-10-25 12:22:45 -0400

    address pr comments

--- lib/shopify-cli/tasks/ensure_loopback_url.rb
+++ lib/shopify-cli/tasks/ensure_loopback_url.rb
@@ -5,16 +5,14 @@ module ShopifyCli
         @ctx = ctx
         api_key = Project.current.env.api_key
         result = Helpers::PartnersAPI.query(ctx, 'get_app_urls', apiKey: api_key)
-        loopback = OAuth::REDIRECT_HOST.to_s
+        loopback = OAuth::REDIRECT_HOST
         app = result['data']['app']
         urls = app['redirectUrlWhitelist']
         if urls.grep(/#{loopback}/).empty?
-          with_loopback = urls.push(loopback.to_s)
+          with_loopback = urls.push(loopback)
           ShopifyCli::Helpers::PartnersAPI.query(@ctx, 'update_whitelisturls', input: {
             redirectUrlWhitelist: with_loopback, apiKey: api_key
           })
-        else
-          return
         end
       end
     end
