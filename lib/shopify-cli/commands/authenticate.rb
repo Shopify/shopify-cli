462e99b577426bb5581454fde57c19d5652d2bcd
Katie Davis <katie.davis@shopify.com>
2019-11-09 10:46:56 -0500

    add ux details to auth task

--- lib/shopify-cli/commands/authenticate.rb
+++ lib/shopify-cli/commands/authenticate.rb
@@ -6,19 +6,28 @@ module ShopifyCli
       def call(args, _name)
         command = args.shift
         ShopifyCli::Tasks::EnsureLoopbackURL.call(@ctx)
-        CLI::UI::Spinner.spin("Requesting access token...") do |spinner|
-          begin
-            case command
-            when 'identity'
-              ShopifyCli::Tasks::AuthenticateIdentity.call(@ctx)
-            else
-              ShopifyCli::Tasks::AuthenticateShopify.call(@ctx)
-            end
-            spinner.update_title("Authetication token stored")
-          rescue OAuth::Error
-            @ctx.puts("{{error:Failed to Authenticate}}")
-            raise(::ShopifyCli::Abort, "Failed to Authenticate")
+        begin
+          case command
+          when 'identity'
+            run_task(ShopifyCli::Tasks::AuthenticateIdentity.call(@ctx))
+          else
+            opt = ask_for_auth
+            opt ? run_task(ShopifyCli::Tasks::AuthenticateShopify.call(@ctx)) : abort
           end
+        rescue OAuth::Error
+          @ctx.puts("{{error:Failed to Authenticate}}")
+          raise(::ShopifyCli::Abort, "Failed to Authenticate")
+        end
+      end
+
+      def ask_for_auth
+        CLI::UI::Prompt.confirm('Open default browser to authenticate with the Partner Dashboard?')
+      end
+
+      def run_task(task_to_run)
+        CLI::UI::Spinner.spin("Waiting to authenticate") do |spinner|
+          spinner.update_title("Authetication token stored")
+          task_to_run
         end
       end
 
