0b226605394a4a0ec3190594d6f53045467f410f
Tyler Ball <tyler@tylerball.net>
2019-11-11 14:51:43 -0500

    add global help flags

--- lib/shopify-cli/sub_command.rb
+++ lib/shopify-cli/sub_command.rb
@@ -8,6 +8,7 @@ module ShopifyCli
         cmd = new(@ctx)
         cmd.options = Options.new
         args = cmd.options.parse(@_options, args[1..-1] || [])
+        return @ctx.puts(cmd.class.help) if cmd.options.help
         cmd.call(args, command_name)
       end
     end
