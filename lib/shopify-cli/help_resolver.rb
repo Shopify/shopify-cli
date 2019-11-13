32371ff62e21350d975f3f9784fdb704b768cda8
Tyler Ball <tyler@tylerball.net>
2019-11-13 13:07:56 -0500

    fix help output when no command is given (#393)

--- lib/shopify-cli/help_resolver.rb
+++ lib/shopify-cli/help_resolver.rb
@@ -4,6 +4,7 @@ module ShopifyCli
   class HelpResolver < CLI::Kit::Resolver
     def call(args)
       args = args.dup
+      return super(args) unless args.first
       if args.first.include?('-h') || args.first.include?('--help')
         help = Commands::Help
         help.ctx = Context.new
