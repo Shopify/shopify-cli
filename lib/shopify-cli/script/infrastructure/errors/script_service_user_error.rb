e7818f5ec911722e7f9af599dbe8da86e4e708d2
Jacob Steves <jacob.steves@shopify.com>
2020-02-04 15:33:03 -0500

    stop ScriptServiceUserError from inheriting from GraphqlError

--- lib/shopify-cli/script/infrastructure/errors/script_service_user_error.rb
+++ lib/shopify-cli/script/infrastructure/errors/script_service_user_error.rb
@@ -3,9 +3,9 @@
 module ShopifyCli
   module ScriptModule
     module Infrastructure
-      class ScriptServiceUserError < GraphqlError
+      class ScriptServiceUserError < StandardError
         def initialize(query_name, errors, variables)
-          super('Script Service', query_name, errors, variables)
+          super("Failed performing #{query_name}. Errors: #{errors}. Variables: #{variables}.")
         end
       end
     end
