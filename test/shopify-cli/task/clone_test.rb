4a7ffd9aaa3d22952316e3a3760b949de0fac89a
Erin Ren <erin.ren@shopify.com>
2020-02-06 07:42:45 -0500

    Fix clone_test
    clone_test was failing because sometimes `test-app` directory already exists in the test/fixture directory.

--- test/shopify-cli/task/clone_test.rb
+++ test/shopify-cli/task/clone_test.rb
@@ -3,6 +3,11 @@ require 'test_helper'
 module ShopifyCli
   module Tasks
     class CloneTest < MiniTest::Test
+      def setup
+        @app_dir = 'test-app'
+        FileUtils.rm_rf(@app_dir) if Dir.exist?(@app_dir)
+      end
+
       def test_clones_git_repo
         CLI::Kit::System.expects(:system).with(
           'git',
@@ -13,7 +18,7 @@ module ShopifyCli
           '--progress'
         ).returns(mock(success?: true))
         capture_io do
-          ShopifyCli::Tasks::Clone.call('git@github.com:shopify/test.git', 'test-app')
+          ShopifyCli::Tasks::Clone.call('git@github.com:shopify/test.git', @app_dir)
         end
       end
 
@@ -28,7 +33,7 @@ module ShopifyCli
             '--progress'
           ).returns(mock(success?: false))
           capture_io do
-            ShopifyCli::Tasks::Clone.call('git@github.com:shopify/test.git', 'test-app')
+            ShopifyCli::Tasks::Clone.call('git@github.com:shopify/test.git', @app_dir)
           end
         end
       end
