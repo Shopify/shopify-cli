9b1717c7acc772ee86da61731b591b8b870992a6
Jacob Steves <jacob.steves@shopify.com>
2020-02-12 11:15:31 -0500

    remove graphql local dependency

--- lib/shopify_cli.rb
+++ lib/shopify_cli.rb
@@ -14,7 +14,7 @@ ENV['PATH'] = ENV['PATH'].split(':').select { |p| p.start_with?('/', '~') }.join
 vendor_path = File.expand_path("../../vendor/lib", __FILE__)
 $LOAD_PATH.unshift(vendor_path) unless $LOAD_PATH.include?(vendor_path)
 
-deps = %w(cli-ui cli-kit smart_properties graphql)
+deps = %w(cli-ui cli-kit smart_properties)
 deps.each do |dep|
   vendor_path = File.expand_path("../../vendor/deps/#{dep}/lib", __FILE__)
   $LOAD_PATH.unshift(vendor_path) unless $LOAD_PATH.include?(vendor_path)
