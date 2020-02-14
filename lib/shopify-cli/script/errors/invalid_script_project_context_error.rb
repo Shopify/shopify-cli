9d3b06ece94512e8de7c64acb2e27bba85272452
Jacob Steves <jacob.steves@shopify.com>
2020-02-14 10:46:34 -0500

    fix linter warnings

--- lib/shopify-cli/script/errors/invalid_script_project_context_error.rb
+++ lib/shopify-cli/script/errors/invalid_script_project_context_error.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ShopifyCli
   module ScriptModule
     class InvalidScriptProjectContextError < StandardError
--- lib/shopify-cli/script/infrastructure/typescript_wasm_test_runner.rb
+++ lib/shopify-cli/script/infrastructure/typescript_wasm_test_runner.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 module ShopifyCli
   module ScriptModule
     module Infrastructure
