f8c6242eef86cc6d87e5c88aa758276e796efe44
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-03-16 13:00:25 -0400

    Removed tunnel as prerequisite task for open

--- lib/project_types/rails/commands/open.rb
+++ lib/project_types/rails/commands/open.rb
@@ -5,8 +5,6 @@ module Rails
     class Open < ShopifyCli::Command
       include ShopifyCli::Helpers::OS
 
-      prerequisite_task :tunnel
-
       def call(*)
         project = ShopifyCli::Project.current
         open_url!(@ctx, "#{project.env.host}/login?shop=#{project.env.shop}")
