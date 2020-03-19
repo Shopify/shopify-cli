81fb427348d2f815db7369df45e1bb6d3f1c5662
Lavender <jumoke.fajinmi@shopify.com>
2020-03-19 15:43:01 -0400

    Associate extension with a parent app project (#8)
    * Associate extension with a parent app project
    
    * Modifications based on pull request comments
    
    * Add helper methods for create_extension tests

--- lib/shopify-cli/commands/create/extension.rb
+++ lib/shopify-cli/commands/create/extension.rb
@@ -1,17 +1,20 @@
 require 'shopify_cli'
+require 'uri'
 
 module ShopifyCli
   module Commands
     class Create
       class Extension < ShopifyCli::SubCommand
         options do |parser, flags|
-          parser.on('--type=TYPE') { |type| flags[:type] = type.downcase }
+          parser.on('--type=TYPE') { |type| flags[:type] = type.downcase  }
+          parser.on('--api-key=KEY') { |key| flags[:api_key] = key.downcase }
         end
 
         def call(args, _name)
           form = Forms::CreateExtension.ask(@ctx, args, options.flags)
           return @ctx.puts(self.class.help) if form.nil?
           build(form.name, @ctx)
+          write_envfile(form)
         end
 
         def self.help
@@ -27,7 +30,7 @@ module ShopifyCli
           ShopifyCli::Tasks::Clone.call('https://github.com/Shopify/shopify-app-extension-template.git', name)
           ShopifyCli::Finalize.request_cd(name)
           ctx.root = File.join(ctx.root, name)
-  
+
           begin
             ctx.rm_r(File.join(ctx.root, '.git'))
             ctx.rm(File.join(ctx.root, 'yarn.lock'))
@@ -36,6 +39,13 @@ module ShopifyCli
           end
 
           ShopifyCli::Tasks::JsDeps.call(ctx)
+        end
+
+        def write_envfile(form)
+          Helpers::EnvFile.new(
+            api_key: form.app["apiKey"],
+            secret: form.app["apiSecretKeys"].first["secret"]
+          ).write(@ctx)
         end
       end
     end
--- test/shopify-cli/forms/create_extension_test.rb
+++ test/shopify-cli/forms/create_extension_test.rb
@@ -7,36 +7,96 @@ module ShopifyCli
       include TestHelpers::Partners
 
       def test_accepts_the_extension_name_as_positional_argument
-        form = CreateExtension.ask(@context, ['test-extension'], type: 'product-details')
+        form = create_extension
         assert_equal form.name, 'test-extension'
       end
 
       def test_accepts_product_details_as_type
-        form = CreateExtension.ask(@context, ['test-extension'], type: 'product-details')
+        form = create_extension
         assert_equal form.type, 'product-details'
       end
 
       def test_accepts_customer_details_as_type
-        form = CreateExtension.ask(@context, ['test-extension'], type: 'customer-details')
+        form = create_extension(type: 'customer-details')
         assert_equal form.type, 'customer-details'
       end
 
       def test_prompts_the_user_to_choose_a_type_if_an_unknown_type_was_provided_as_flag
-        CLI::UI::Prompt.expects(:ask).with('What type of extension would you like to create?')
-
         io = capture_io do
-          CreateExtension.ask(@context, ['test-extension'], type: 'unknown-type')
-        end 
+          create_extension(type: 'unknown-type')
+        end
 
-        assert_match('Invalid Extension Type.', io.join)
+        assert_match('Invalid extension type.', io.join)
       end
 
-      def test_prompts_the_user_to_choose_a_type_if_an_no_type_was_provided
+      def test_prompts_the_user_to_choose_a_type_if_no_type_was_provided
         CLI::UI::Prompt.expects(:ask).with('What type of extension would you like to create?')
 
         capture_io do
-          CreateExtension.ask(@context, ['test-extension'], type: nil)
-        end 
+          create_extension(type: nil)
+        end
+      end
+
+      def test_accepts_the_api_key_to_associate_with_extension
+        form = create_extension
+        orgs = Helpers::Organizations.fetch_with_app(@context)
+        assert_equal form.app['apiKey'], orgs.first['apps'].first['apiKey']
+      end
+
+      def test_prompts_the_user_to_choose_an_app_to_associate_with_extension_if_no_app_is_provided
+        CLI::UI::Prompt.expects(:ask).with('Which app will you like to associate with the extension?')
+
+        capture_io do
+          create_extension(api_key: nil)
+        end
+      end
+
+      def test_fails_with_invalid_api_key_to_associate_with_extension
+        io = capture_io do
+          form = create_extension(api_key: '00001')
+        end
+
+        assert_match('The api key does not match any of the existing apps', io.join)
+      end
+
+      private
+
+      def create_extension(context = @context, name = ['test-extension'], type: 'product-details', api_key: '1234')
+        stub_query_for_fetch_organizations_with_apps
+        CreateExtension.ask(context, name, type: type, api_key: api_key)
+      end
+
+      def stub_query_for_fetch_organizations_with_apps
+        stub_partner_req(
+          'all_orgs_with_apps',
+          resp: {
+            data: {
+              organizations: {
+                nodes: [
+                  {
+                    'id': 421,
+                    'businessName': "one",
+                    'stores': {
+                      'nodes': [
+                        { 'shopDomain': 'store.myshopify.com' },
+                      ],
+                    },
+                    'apps': {
+                      nodes: [{
+                        id: 123,
+                        title: 'app',
+                        'apiKey': '1234',
+                        'apiSecretKeys': [{
+                          'secret': "1233",
+                        }],
+                      }],
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        )
       end
     end
   end
