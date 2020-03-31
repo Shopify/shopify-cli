6b0465a4a533e4637b4b7774657da4e0ec76c22a
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-03-31 17:55:14 -0400

    Convert tunnel cmd to Project Type-based system - added Tunnel to cli.rb files under {node,rails} directories. - copied and modified tunnel.rb to {node,rails}/commands directories. - copied and modified tunnel_test.rb to {nodes,rails} test directories. - deleted tunnel.rb and tunnel_test.rb from respective directories under   previous system.

---
+++ lib/project_types/rails/commands/tunnel.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'shopify_cli'
+
+module Rails
+  module Commands
+    class Tunnel < ShopifyCli::Command
+      # subcommands :auth, :start, :stop
+
+      def call(args, _name)
+        subcommand = args.shift
+        task = ShopifyCli::Tasks::Tunnel.new
+        case subcommand
+        when 'auth'
+          token = args.shift
+          task.auth(@ctx, token)
+        when 'start'
+          task.call(@ctx)
+        when 'stop'
+          task.stop(@ctx)
+        else
+          @ctx.puts(self.class.help)
+        end
+      end
+
+      def self.help
+        <<~HELP
+          Start or stop an http tunnel to your local development app using ngrok.
+            Usage: {{command:#{ShopifyCli::TOOL_NAME} tunnel [ auth | start | stop ]}}
+        HELP
+      end
+
+      def self.extended_help
+        <<~HELP
+          {{bold:Subcommands:}}
+
+            {{cyan:auth}}: Writes an ngrok auth token to ~/.ngrok2/ngrok.yml to allow connecting with an ngrok account. Visit https://dashboard.ngrok.com/signup to sign up.
+              Usage: {{command:#{ShopifyCli::TOOL_NAME} tunnel auth <token>}}
+
+            {{cyan:start}}: Starts an ngrok tunnel, will print the URL for an existing tunnel if already running.
+              Usage: {{command:#{ShopifyCli::TOOL_NAME} tunnel start}}
+
+            {{cyan:stop}}: Stops the ngrok tunnel.
+              Usage: {{command:#{ShopifyCli::TOOL_NAME} tunnel stop}}
+
+        HELP
+      end
+    end
+  end
+end
