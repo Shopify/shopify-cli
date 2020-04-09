daa0c6f0c6d2070f43e18430f9788544e7f5e012
Tim Anema <tim.anema@shopify.com>
2020-04-09 12:41:09 -0400

    linting

--- test/test_helpers/schema.rb
+++ test/test_helpers/schema.rb
@@ -2,7 +2,8 @@ module TestHelpers
   module Schema
     def setup
       super
-      schema = ShopifyCli::AdminAPI::Schema[JSON.parse(File.read(File.join(ShopifyCli::ROOT, "test/fixtures/shopify_schema.json")))]
+      json_data = File.read(File.join(ShopifyCli::ROOT, "test/fixtures/shopify_schema.json"))
+      schema = ShopifyCli::AdminAPI::Schema[JSON.parse(json_data)]
       ShopifyCli::AdminAPI::Schema.stubs(:get).returns(schema)
     end
   end
