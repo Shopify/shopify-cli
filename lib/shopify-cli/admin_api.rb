a7d850280982e2b2b188183a802983611e61d38d
Tim Anema <tim.anema@shopify.com>
2020-04-22 14:21:16 -0400

    Simplifying Api Classes

--- lib/shopify-cli/admin_api.rb
+++ lib/shopify-cli/admin_api.rb
@@ -7,45 +7,61 @@ module ShopifyCli
 
     class << self
       def query(ctx, body, api_version: nil, shop: nil, **variables)
-        @shop = shop
-        authenticated_req(ctx) do
-          api_client(ctx, api_version).query(body, variables: variables)
+        shop = shop || Project.current.env.shop
+        authenticated_req(ctx, shop) do
+          api_client(ctx, api_version, shop).query(body, variables: variables)
         end
       end
 
       private
 
-      def authenticated_req(ctx)
+      def authenticated_req(ctx, shop)
         yield
       rescue API::APIRequestUnauthorizedError
-        Tasks::AuthenticateShopify.call(ctx, shop: @shop)
+        authenticate(ctx, shop)
         retry
       end
 
-      def api_client(ctx, api_version)
+      def authenticate(ctx, shop)
+        env = Project.current.env
+        ShopifyCli::OAuth.new(
+          ctx: ctx,
+          service: 'admin',
+          client_id: env.api_key,
+          secret: env.secret,
+          scopes: env.scopes,
+          token_path: "/access_token",
+          options: { 'grant_options[]' => 'per user' },
+        ).authenticate("https://#{shop}/admin/oauth")
+      end
+
+      def api_client(ctx, api_version, shop)
         new(
           ctx: ctx,
           auth_header: 'X-Shopify-Access-Token',
-          token: Resources::Tokens.admin(ctx),
-          url: "#{endpoint}/#{fetch_api_version(ctx, api_version)}/graphql.json",
+          token: admin_access_token(ctx, shop),
+          url: "https://#{shop}/admin/api/#{fetch_api_version(ctx, api_version, shop)}/graphql.json",
         )
       end
 
-      def fetch_api_version(ctx, api_version)
+      def admin_access_token(ctx, shop)
+        ShopifyCli::DB.get(:admin_access_token) do
+          authenticate(ctx, shop)
+          ShopifyCli::DB.get(:admin_access_token)
+        end
+      end
+
+      def fetch_api_version(ctx, api_version, shop)
         return api_version unless api_version.nil?
         client = new(
           ctx: ctx,
           auth_header: 'X-Shopify-Access-Token',
-          token: Resources::Tokens.admin(ctx),
-          url: "#{endpoint}/unstable/graphql.json",
+          token: admin_access_token(ctx, shop),
+          url: "https://#{shop}/admin/api/unstable/graphql.json",
         )
         versions = client.query('api_versions')['data']['publicApiVersions']
         latest = versions.find { |version| version['displayName'].include?('Latest') }
         latest['handle']
-      end
-
-      def endpoint
-        "https://#{@shop || Project.current.env.shop}/admin/api"
       end
     end
   end
