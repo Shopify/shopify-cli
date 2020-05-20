0951fcc2bd8adf6514ff5ce64a05cad44e8bb961
Erik Ryhorchuk <erik.ryhorchuk@shopify.com>
2020-05-20 12:33:58 -0700

    fix TravisCL style issues

--- lib/project_types/script/commands/create.rb
+++ lib/project_types/script/commands/create.rb
@@ -30,7 +30,7 @@ module Script
       end
 
       def self.help
-        allowed_values = Script::Layers::Application::ExtensionPoints.types.map {|type| "{{cyan:#{type}}}"}
+        allowed_values = Script::Layers::Application::ExtensionPoints.types.map { |type| "{{cyan:#{type}}}" }
         ShopifyCli::Context.message('script.create.help', ShopifyCli::TOOL_NAME, allowed_values.join(', '))
       end
     end
