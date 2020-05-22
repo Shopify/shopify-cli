8f8324b23564e61d0c9494f3e2184dde347fd8fd
Paulo Margarido <paulo.margarido@shopify.com>
2020-05-22 15:42:52 -0400

    Committing a non-change to clear a Travis CI task failure to unblock the gem development

--- lib/project_types/node/messages/messages.rb
+++ lib/project_types/node/messages/messages.rb
@@ -41,7 +41,7 @@ module Node
           info: {
             created: "{{v}} {{green:%s}} was created in your Partner Dashboard {{underline:%s}}",
             serve: "{{*}} Change into your new project folder {{green:%s}} and run {{command:%s serve}} to start a " \
-              "local server",
+              "local server", 
             install: "{{*}} Then, visit {{underline:%s/test}} to install {{green:%s}} on your Dev Store",
           },
           node_version: "node %s",
