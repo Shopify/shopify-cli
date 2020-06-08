36e8dbbb41789d4b2576da8b65652e59f19c75d8
Erik Ryhorchuk <64974039+erik-shopify@users.noreply.github.com>
2020-06-08 10:09:46 -0700

    Rewrite enable and deploy command descriptions (#622)

--- test/project_types/script/commands/enable_test.rb
+++ test/project_types/script/commands/enable_test.rb
@@ -31,8 +31,56 @@ module Script
           extension_point_type: @ep_type,
           title: @script_name
         )
+
+        @context
+          .expects(:puts)
+          .with(@context.message(
+            'script.enable.script_enabled',
+            api_key: @api_key,
+            shop_domain: @shop_domain,
+            type: @ep_type.capitalize,
+            title: @script_name
+          ))
+
+        @context
+          .expects(:puts)
+          .with(@context.message('script.enable.info'))
+
         capture_io do
           perform_command
+        end
+      end
+
+      def test_calls_application_enable_error
+        Script::Layers::Application::EnableScript.expects(:call).with(
+          ctx: @context,
+          api_key: @api_key,
+          shop_domain: @shop_domain,
+          configuration: @configuration,
+          extension_point_type: @ep_type,
+          title: @script_name
+        ).raises(StandardError)
+
+        @context
+          .expects(:puts)
+          .with(@context.message(
+            'script.enable.script_enabled',
+            api_key: @api_key,
+            shop_domain: @shop_domain,
+            type: @ep_type.capitalize,
+            title: @script_name
+          ))
+          .never
+
+        @context
+          .expects(:puts)
+          .with(@context.message('script.enable.info'))
+          .never
+
+        assert_raises StandardError do
+          capture_io do
+            perform_command
+          end
         end
       end
 
