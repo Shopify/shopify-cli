5e2a5d3223882b4d96d56bd542d4a91a93b027d3
Erik Ryhorchuk <64974039+erik-shopify@users.noreply.github.com>
2020-06-08 06:53:26 -0700

    Cleanup runtime errors during script creation (#638)

--- test/project_types/script/commands/create_test.rb
+++ test/project_types/script/commands/create_test.rb
@@ -46,6 +46,30 @@ module Script
         Script::Commands::Create.help
       end
 
+      def test_cleanup_after_error
+        Dir.mktmpdir(@script_name)
+        Layers::Application::CreateScript.expects(:call).with(
+          ctx: @context,
+          language: @language,
+          script_name: @script_name,
+          extension_point_type: @ep_type
+        ).raises(StandardError)
+
+        ScriptProject.expects(:cleanup).with(
+          ctx: @context,
+          script_name: @script_name,
+          root_dir: @context.root
+        )
+
+        assert_raises StandardError do
+          capture_io do
+            perform_command
+          end
+        end
+
+        refute Dir.exist?(@script_name)
+      end
+
       private
 
       def perform_command
