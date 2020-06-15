f28f25dcb871f0c4487ffe812a44f163f0367038
Fred Ulrich <fred.ulrich@shopify.com>
2020-06-15 16:37:26 -0400

    Updating JsDeps to improve install failure handling

--- test/shopify-cli/js_deps_test.rb
+++ test/shopify-cli/js_deps_test.rb
@@ -7,31 +7,66 @@ module ShopifyCli
       project_context('app_types', 'node')
     end
 
-    def test_installs_with_npm
+    def test_installs_with_npm_and_returns_true
       JsSystem.any_instance.stubs(:yarn?).returns(false)
       CLI::Kit::System.expects(:system).with(
         'npm', 'install', '--no-audit', '--no-optional', '--silent',
         env: @context.env,
         chdir: @context.root,
-      )
+      ).returns(mock(success?: true))
+
       io = capture_io do
-        JsDeps.install(@context)
+        assert JsDeps.install(@context)
       end
+
       output = io.join
       assert_match('Installing dependencies with npm...', output)
     end
 
-    def test_installs_with_yarn
+    def test_install_with_npm_outputs_an_error_message_if_install_fails_and_returns_false
+      JsSystem.any_instance.stubs(:yarn?).returns(false)
+      CLI::Kit::System.expects(:system).with(
+        'npm', 'install', '--no-audit', '--no-optional', '--silent',
+        env: @context.env,
+        chdir: @context.root,
+      ).returns(mock(success?: false))
+
+      io = capture_io do
+        refute JsDeps.install(@context)
+      end
+
+      output = io.join
+      assert_match(@context.message('core.js_deps.error.install_error'), output)
+    end
+
+    def test_installs_with_yarn_and_returns_true
       JsSystem.any_instance.stubs(:yarn?).returns(true)
       CLI::Kit::System.expects(:system).with(
         'yarn', 'install', '--silent',
         chdir: @context.root
       ).returns(mock(success?: true))
+
       io = capture_io do
-        JsDeps.install(@context)
+        assert JsDeps.install(@context)
       end
+
       output = io.join
       assert_match('Installing dependencies with yarn...', output)
+    end
+
+    def test_install_with_yarn_outputs_an_error_message_if_install_fails_and_returns_false
+      JsSystem.any_instance.stubs(:yarn?).returns(true)
+      CLI::Kit::System.expects(:system).with(
+        'yarn', 'install', '--silent',
+        chdir: @context.root
+      ).returns(mock(success?: false))
+
+      io = capture_io do
+        refute JsDeps.install(@context)
+      end
+
+      output = io.join
+      assert_match(@context.message('core.js_deps.error.install_error'), output)
     end
   end
 end
