6c03ff0c82d2af37f99392fc003c40f57d1a6ae8
Paulo Margarido <paulo.margarido@shopify.com>
2020-06-16 13:41:50 -0400

    Revisit the deprecation message and check for the FD instead of a function to make the check easier across shells

--- shopify.fish
+++ shopify.fish
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/usr/bin/env fish
+# vi: ft=fish
 
 # If we're not already in a shim environment but still tried to set up a shim, print out the re-installation warning
-typeset -f shopify > /dev/null 2>&1
-if [ "$?" != "0" ]; then
+type -t shopify > /dev/null 2>&1
+if [ $status != "0" ]
   echo "This version of Shopify App CLI is no longer supported. You'll need to upgrade to continue using it, this process typically takes a few minutes.
     Please visit https://shopify.github.io/shopify-app-cli/upgrade/ for complete instructions."
-fi
+end
