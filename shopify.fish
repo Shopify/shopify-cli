c89be852cea2c0ecec34607b900cdcd0ec2d6b4c
Paulo Margarido <paulo.margarido@shopify.com>
2020-06-16 15:21:42 -0400

    Revamping messages based on UX input

--- shopify.fish
+++ shopify.fish
@@ -4,6 +4,8 @@
 # If we're not already in a shim environment but still tried to set up a shim, print out the re-installation warning
 type -t shopify > /dev/null 2>&1
 if [ $status != "0" ]
-  echo "This version of Shopify App CLI is no longer supported. You'll need to upgrade to continue using it, this process typically takes a few minutes.
-    Please visit https://shopify.github.io/shopify-app-cli/upgrade/ for complete instructions."
+  echo "This version of Shopify App CLI is no longer supported. You’ll need to upgrade to continue using it. This process typically takes a few minutes.
+Please visit this page for complete instructions:
+  https://shopify.github.io/shopify-app-cli/upgrade/
+"
 end
