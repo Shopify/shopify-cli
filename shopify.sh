5816da33142f182887559cbfda9f49b42214dc55
Paulo Margarido <paulo.margarido@shopify.com>
2020-06-22 12:32:44 -0400

    Remove mentions of the upgrade time from the warnings

--- shopify.sh
+++ shopify.sh
@@ -3,7 +3,7 @@
 # If we're not already in a shim environment but still tried to set up a shim, print out the re-installation warning
 typeset -f shopify > /dev/null 2>&1
 if [ "$?" != "0" ]; then
-  echo "This version of Shopify App CLI is no longer supported. You’ll need to upgrade to continue using it. This process typically takes a few minutes.
+  echo "This version of Shopify App CLI is no longer supported. You’ll need to upgrade to continue using it.
 Please visit this page for complete instructions:
   https://shopify.github.io/shopify-app-cli/upgrade/
 "
