f075c89f8999fb13732b95d34821efa8bbcf1ad5
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-06-22 11:17:21 -0400

    Fix delete of symlink

--- lib/rubygems_plugin.rb
+++ lib/rubygems_plugin.rb
@@ -3,7 +3,15 @@ Gem.post_uninstall do |uninstaller|
     require 'fileutils'
 
     symlink = '/usr/local/bin/shopify-cli'
-    system("sudo rm -f #{symlink}") if File.symlink?(symlink)
+
+    # delete the symbolic link IFF it exists AND it does not point to a file
+    # (i.e., it's been left hanging as a result of the uninstall, as expected)
+    #
+    # if the file still exists, either the uninstall failed (possible but
+    # unlikely) OR
+    # there's another installation of the gem in another ruby folder that has
+    # overwritten it, so leave the symbolic link alone
+    system("sudo rm -f #{symlink}") if File.symlink?(symlink) && !File.exist?(symlink)
   end
 
   true
