69127ba78bd7d39641480daa1cb422df025c65e2
Paulo Margarido <paulo.margarido@shopify.com>
2020-07-20 13:50:45 -0400

    Remove autoload reference to extinct update command

--- lib/shopify-cli/core.rb
+++ lib/shopify-cli/core.rb
@@ -4,6 +4,5 @@ module ShopifyCli
     autoload :Executor, 'shopify-cli/core/executor'
     autoload :HelpResolver, 'shopify-cli/core/help_resolver'
     autoload :Monorail, 'shopify-cli/core/monorail'
-    autoload :Update, 'shopify-cli/core/update'
   end
 end
