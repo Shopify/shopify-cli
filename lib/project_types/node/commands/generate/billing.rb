ba22e82d2e3002be93ed15dd58f854d4a9b65f46
Paulo Margarido <paulo.margarido@shopify.com>
2020-07-29 15:40:42 -0400

    Adapt node commands for Windows

--- lib/project_types/node/commands/generate/billing.rb
+++ lib/project_types/node/commands/generate/billing.rb
@@ -5,8 +5,8 @@ module Node
     class Generate
       class Billing < ShopifyCli::SubCommand
         BILLING_TYPES = {
-          'recurring-billing' => './node_modules/.bin/generate-node-app recurring-billing',
-          'one-time-billing' => './node_modules/.bin/generate-node-app one-time-billing',
+          'recurring-billing' => ['./node_modules/.bin/generate-node-app', 'recurring-billing'],
+          'one-time-billing' => ['./node_modules/.bin/generate-node-app', 'one-time-billing'],
         }
         def call(args, _name)
           selected_type = BILLING_TYPES[args[1]]
@@ -18,11 +18,12 @@ module Node
             end
           end
           billing_type_name = BILLING_TYPES.key(selected_type)
+          selected_type[0] = File.join(ShopifyCli::Project.current.directory, selected_type[0])
+          selected_type = selected_type.join(' ')
+
           spin_group = CLI::UI::SpinGroup.new
           spin_group.add(@ctx.message('node.generate.billing.generating', billing_type_name)) do |spinner|
-            Node::Commands::Generate.run_generate(
-              selected_type, billing_type_name, @ctx
-            )
+            Node::Commands::Generate.run_generate(selected_type, billing_type_name, @ctx)
             spinner.update_title(@ctx.message('node.generate.billing.generated', billing_type_name))
           end
           spin_group.wait
