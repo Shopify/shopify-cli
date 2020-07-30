97f296f8710f14654a720fb332fce661ea62fe2c
Erik Ryhorchuk <erik.ryhorchuk@shopify.com>
2020-07-30 00:00:10 -0700

    keep script dir if create with same name

--- lib/project_types/script/commands/create.rb
+++ lib/project_types/script/commands/create.rb
@@ -26,6 +26,8 @@ module Script
           extension_point_type: form.extension_point
         )
         @ctx.puts(@ctx.message('script.create.change_directory_notice', project.script_name))
+      rescue Script::Errors::ScriptProjectAlreadyExistsError => e
+        UI::ErrorHandler.pretty_print_and_raise(e, failed_op: @ctx.message('script.create.error.operation_failed'))
       rescue StandardError => e
         ScriptProject.cleanup(ctx: @ctx, script_name: form.name, root_dir: cur_dir) if form
         UI::ErrorHandler.pretty_print_and_raise(e, failed_op: @ctx.message('script.create.error.operation_failed'))
