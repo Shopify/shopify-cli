3c7ddfb62d1088e69ba58e2e0fa9d85397b9ebf4
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2020-08-12 14:31:53 -0400

    Vagrant VM provisioning for Linux testing - Ubuntu with 3 configs - chruby, rbenv, rvm

---
+++ test/linux/provisioning/ubuntu-focal-common.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+# update linux system
+echo "################################################################################"
+echo "### Updating/upgrading system packages..."
+echo "################################################################################"
+sudo apt-get --quiet --yes update
+sudo apt-get --quiet --yes upgrade
+
+# install necessary packages for installing/using shopify-app-cli
+echo "################################################################################"
+echo "### Installing build-essential, unzip, libsqlite3-dev, libmysqlclient-dev, mysql-server..."
+echo "################################################################################"
+sudo apt-get --quiet --yes install build-essential
+sudo apt-get --quiet --yes install unzip
+sudo apt-get --quiet --yes install libsqlite3-dev
+sudo apt-get --quiet --yes install libmysqlclient-dev
+sudo apt-get --quiet --yes install mysql-server
+
+# install latest version of node
+echo "################################################################################"
+echo "### Installing latest version of node/npm..."
+echo "################################################################################"
+curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt-get --quiet --yes install nodejs
+
+# install yarn
+cd /home/vagrant
+echo "################################################################################"
+echo "### Installing yarn..."
+echo "################################################################################"
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get --quiet --yes update && sudo apt-get --quiet --yes install yarn
+
+# download a clone of the shopify-app-cli repo
+cd /home/vagrant
+echo "################################################################################"
+echo "### Cloning shopify-app-cli..."
+echo "################################################################################"
+mkdir src && cd src && git clone https://github.com/Shopify/shopify-app-cli.git && cd /home/vagrant
+echo "################################################################################"
+echo "### Clone of shopify-app-cli located at ~/src/shopify-app-cli"
+echo "################################################################################"
