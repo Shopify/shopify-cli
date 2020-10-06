ccf34a1349ef28a019a27a11d18293216ffaca40
Melanie Wang <melanie.wang@shopify.com>
2020-10-06 14:11:25 -0400

    Use tmpdir to test dev feature

--- test/shopify-cli/shopifolk_test.rb
+++ test/shopify-cli/shopifolk_test.rb
@@ -6,18 +6,17 @@ module ShopifyCli
     DEV_FEATURE_NAME = 'dev_shopifolk'
 
     def test_correct_features_is_shopifolk
+      gcloud_path = '../shopifolk_correct.conf'
       ShopifyCli::Feature.disable(GCLOUD_FEATURE_NAME.to_s)
       ShopifyCli::Feature.disable(DEV_FEATURE_NAME.to_s)
-      ShopifyCli::Shopifolk.check
+      Dir.mktmpdir do |dev_dir|
+        FileUtils.mkdir_p("#{dev_dir}/bin")
+        FileUtils.touch("#{dev_dir}/bin/dev")
+        FileUtils.touch("#{dev_dir}/.shopify-build")
+        ShopifyCli::Shopifolk.new.shopifolk?(gcloud_path, dev_dir)
+      end
       assert ShopifyCli::Config.get_bool(Feature::SECTION, GCLOUD_FEATURE_NAME.to_s)
       assert ShopifyCli::Config.get_bool(Feature::SECTION, DEV_FEATURE_NAME.to_s)
-    end
-
-    def test_correct_gcloud_enables_gcloud_shopifolk_feature
-      path = '../shopifolk_correct.conf'
-      ShopifyCli::Feature.disable(GCLOUD_FEATURE_NAME.to_s)
-      ShopifyCli::Shopifolk.new.shopifolk?(path)
-      assert ShopifyCli::Config.get_bool(Feature::SECTION, GCLOUD_FEATURE_NAME.to_s)
     end
 
     def test_incorrect_gcloud_disables_shopifolk_feature
@@ -35,12 +34,6 @@ module ShopifyCli
       path_no_email = '../shopifolk_incorrect_no_email.conf'
       ShopifyCli::Shopifolk.new.shopifolk?(path_no_email)
       refute ShopifyCli::Config.get_bool(Feature::SECTION, GCLOUD_FEATURE_NAME.to_s)
-    end
-
-    def test_correct_dev_path_enables_dev_shopifolk_feature
-      ShopifyCli::Feature.disable(DEV_FEATURE_NAME.to_s)
-      ShopifyCli::Shopifolk.new.shopifolk?
-      assert ShopifyCli::Config.get_bool(Feature::SECTION, DEV_FEATURE_NAME.to_s)
     end
 
     def test_incorrect_dev_path_disables_dev_shopifolk_feature
