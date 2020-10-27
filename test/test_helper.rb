357246b406d8fdda141314420213358ce8ae81a2
Andy Waite <andyw8@users.noreply.github.com>
2020-10-27 10:45:31 -0400

    Remove unnecessary load path manpipulation in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,3 @@
-begin
-  addpath = lambda do |p|
-    path = File.expand_path("../../#{p}", __FILE__)
-    $LOAD_PATH.unshift(path) unless $LOAD_PATH.include?(path)
-  end
-  addpath.call("lib")
-  addpath.call("vendor/lib")
-end
-
 ENV['RUNNING_SHOPIFY_CLI_TESTS'] = 1.to_s
 
 require 'rubygems'
