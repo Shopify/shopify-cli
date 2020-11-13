df056296b07c5e0e32d441decf0a0a78b078af34
Carmela Leung <carmela.leung@shopify.com>
2020-11-13 11:43:13 -0500

    Refactor HTTP request

--- lib/shopify-cli/http_request.rb
+++ lib/shopify-cli/http_request.rb
@@ -2,19 +2,26 @@ require 'net/http'
 
 module ShopifyCli
   class HttpRequest
-    def self.call(uri, body, headers, method)
-      http = ::Net::HTTP.new(uri.host, uri.port)
-      http.use_ssl = true
-
-      req = if method == "POST"
-        ::Net::HTTP::Post.new(uri.request_uri)
-      elsif method == "GET"
-        ::Net::HTTP::Get.new(uri.request_uri)
+    class << self
+      def post(uri, body, headers)
+        req = ::Net::HTTP::Post.new(uri.request_uri)
+        request(uri, body, headers, req)
       end
-      req.body = body unless body.nil?
-      req['Content-Type'] = 'application/json'
-      headers.each { |header, value| req[header] = value }
-      http.request(req)
+
+      def get(uri, body, headers)
+        req = ::Net::HTTP::Get.new(uri.request_uri)
+        request(uri, body, headers, req)
+      end
+
+      def request(uri, body, headers, req)
+        http = ::Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+
+        req.body = body unless body.nil?
+        req['Content-Type'] = 'application/json'
+        headers.each { |header, value| req[header] = value }
+        http.request(req)
+      end
     end
   end
 end
