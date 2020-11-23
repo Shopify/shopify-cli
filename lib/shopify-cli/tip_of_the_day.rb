822019add1e8e53a0fbe4333f007e77327209d48
Melanie Wang <melanie.wang@shopify.com>
2020-11-23 12:22:27 -0500

    Add base tip file

--- lib/shopify-cli/tip_of_the_day.rb
+++ lib/shopify-cli/tip_of_the_day.rb
@@ -1,7 +1,20 @@
+require 'json'
+
 module ShopifyCli
   class TipOfTheDay
     def self.call
-      puts "h"
+      puts random_tip
+    end
+
+    def self.read_file
+      path = File.expand_path(ShopifyCli::ROOT + '/lib/tips.json')
+      file = File.read(path)
+      JSON.parse(file)['tips']
+    end
+
+    def self.random_tip
+      tips = read_file
+      tips.sample["text"]
     end
   end
 end
