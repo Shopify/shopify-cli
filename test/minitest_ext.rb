29c6847479932f572f0afd1fe9cf4201e4142e1f
Emily Harber <emily.harber@shopify.com>
2020-11-25 10:52:20 -0500

    Stubbing tip of the day for tests

--- test/minitest_ext.rb
+++ test/minitest_ext.rb
@@ -18,6 +18,9 @@ module Minitest
       end
       project_context('project')
       ::ShopifyCli::Project.clear
+      ShopifyCli::Config.stubs(:get_section).with("tipoftheday").returns('enabled' => 'false')
+      ShopifyCli::Config.stubs(:get_bool).with("tipoftheday", "enabled").returns(false)
+      ShopifyCli::Config.stubs(:get_section).with("tiplog").returns({})
       super
     end
 
@@ -51,6 +54,7 @@ module Minitest
     def run_cmd(cmd, split_cmd = true)
       stub_prompt_for_cli_updates
       stub_new_version_check
+      # stub_tip_of_the_day_call
 
       new_cmd = split_cmd ? cmd.split : cmd
       ShopifyCli::Core::EntryPoint.call(new_cmd, @context)
@@ -81,6 +85,9 @@ module Minitest
 
     def stub_prompt_for_cli_updates
       ShopifyCli::Config.stubs(:get_section).with("autoupdate").returns('enabled' => 'true')
+      # ShopifyCli::Config.stubs(:get_section).with("tipoftheday").returns('enabled' => 'false')
+      # ShopifyCli::Config.stubs(:get_bool).with("tipoftheday", "enabled").returns(false)
+      # ShopifyCli::Config.stubs(:get_section).with("tiplog").returns({})
     end
 
     def stub_new_version_check
@@ -93,5 +100,13 @@ module Minitest
         })
         .to_return(status: 200, body: "{\"version\":\"#{ShopifyCli::VERSION}\"}", headers: {})
     end
+
+    def stub_tip_of_the_day_call
+      FakeFS::FileSystem.clone(ShopifyCli::ROOT + '/test/fixtures/tips.json')
+      @tips_path = File.expand_path(ShopifyCli::ROOT + '/test/fixtures/tips.json')
+
+      stub_request(:get, "https://gist.githubusercontent.com/andyw8/c772d254b381789f9526c7b823755274/raw/4b227372049d6a6e5bb7fa005f261c4570c53229/tips.json")
+        .to_return(status: 200, body: File.read(@tips_path), headers: {})
+    end 
   end
 end
