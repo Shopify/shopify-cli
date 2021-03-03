44aba9f095f3243058633f459909e1cf454aca09
Melanie Wang <melanie.wang@shopify.com>
2021-03-03 13:01:30 -0500

    Login command

---
+++ lib/shopify-cli/commands/login.rb
@@ -0,0 +1,15 @@
+require 'shopify_cli'
+
+module ShopifyCli
+  module Commands
+    class Login < ShopifyCli::Command
+      def call(*)
+        IdentityAuth.new(ctx: @ctx).authenticate
+      end
+
+      def self.help
+        ShopifyCli::Context.message('core.login.help', ShopifyCli::TOOL_NAME)
+      end
+    end
+  end
+end
