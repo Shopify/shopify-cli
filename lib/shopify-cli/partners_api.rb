cb691ade3264fbe98000d05ee699936c7f2ab9d7
Melanie Wang <melanie.wang@shopify.com>
2021-03-05 14:35:53 -0500

    Use new identity auth

--- lib/shopify-cli/partners_api.rb
+++ lib/shopify-cli/partners_api.rb
@@ -80,35 +80,14 @@ module ShopifyCli
       end
 
       def access_token(ctx)
-        ShopifyCli::DB.get(:identity_exchange_token) do
+        ShopifyCli::DB.get(:partners_exchange_token) do
           authenticate(ctx)
-          ShopifyCli::DB.get(:identity_exchange_token)
+          ShopifyCli::DB.get(:partners_exchange_token)
         end
       end
 
       def authenticate(ctx)
-        OAuth.new(
-          ctx: ctx,
-          service: "identity",
-          client_id: cli_id,
-          scopes: scopes.join(" "),
-          request_exchange: partners_id,
-        ).authenticate("#{auth_endpoint}/oauth")
-      end
-
-      def partners_id
-        return "271e16d403dfa18082ffb3d197bd2b5f4479c3fc32736d69296829cbb28d41a6" if ENV[LOCAL_DEBUG].nil?
-        "df89d73339ac3c6c5f0a98d9ca93260763e384d51d6038da129889c308973978"
-      end
-
-      def cli_id
-        return "fbdb2649-e327-4907-8f67-908d24cfd7e3" if ENV[LOCAL_DEBUG].nil?
-        "e5380e02-312a-7408-5718-e07017e9cf52"
-      end
-
-      def auth_endpoint
-        return "https://accounts.shopify.com" if ENV[LOCAL_DEBUG].nil?
-        "https://identity.myshopify.io"
+        IdentityAuth.new(ctx: ctx).authenticate
       end
 
       def endpoint
@@ -123,12 +102,6 @@ module ShopifyCli
           "partners.shopify.com"
         end
         "https://#{domain}"
-      end
-
-      def scopes
-        %w[openid https://api.shopify.com/auth/partners.app.cli.access].tap do |result|
-          result << "employee" if ShopifyCli::Shopifolk.acting_as_shopify_organization?
-        end
       end
     end
 
