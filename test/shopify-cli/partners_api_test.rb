186064132b488c48ebb7a4439bc1acf8b4912257
Melanie Wang <melanie.wang@shopify.com>
2021-03-05 14:35:53 -0500

    Use new identity auth

--- test/shopify-cli/partners_api_test.rb
+++ test/shopify-cli/partners_api_test.rb
@@ -5,7 +5,7 @@ module ShopifyCli
     include TestHelpers::Project
 
     def test_query_calls_partners_api
-      ShopifyCli::DB.expects(:get).with(:identity_exchange_token).returns("token123")
+      ShopifyCli::DB.expects(:get).with(:partners_exchange_token).returns('token123')
       api_stub = stub
       PartnersAPI.expects(:new).with(
         ctx: @context,
@@ -18,7 +18,7 @@ module ShopifyCli
 
     def test_query_can_reauth
       Shopifolk.stubs(:check).returns(false)
-      ShopifyCli::DB.expects(:get).with(:identity_exchange_token).returns("token123").twice
+      ShopifyCli::DB.expects(:get).with(:partners_exchange_token).returns('token123').twice
 
       api_stub = stub
       PartnersAPI.expects(:new).with(
@@ -30,24 +30,17 @@ module ShopifyCli
       api_stub.expects(:query).raises(API::APIRequestUnauthorizedError)
 
       @oauth_client = mock
-      ShopifyCli::OAuth
+      ShopifyCli::IdentityAuth
         .expects(:new)
-        .with(
-          ctx: @context,
-          service: "identity",
-          client_id: "fbdb2649-e327-4907-8f67-908d24cfd7e3",
-          scopes: "openid https://api.shopify.com/auth/partners.app.cli.access",
-          request_exchange: "271e16d403dfa18082ffb3d197bd2b5f4479c3fc32736d69296829cbb28d41a6",
-        ).returns(@oauth_client)
+        .with(ctx: @context).returns(@oauth_client)
       @oauth_client
         .expects(:authenticate)
-        .with("https://accounts.shopify.com/oauth")
 
       PartnersAPI.query(@context, "query")
     end
 
     def test_query_fails_gracefully_without_partners_account
-      ShopifyCli::DB.expects(:get).with(:identity_exchange_token).returns("token123")
+      ShopifyCli::DB.expects(:get).with(:partners_exchange_token).returns('token123')
       api_stub = stub
       PartnersAPI.expects(:new).with(
         ctx: @context,
@@ -60,7 +53,7 @@ module ShopifyCli
     end
 
     def test_query
-      ShopifyCli::DB.expects(:get).with(:identity_exchange_token).returns("token123")
+      ShopifyCli::DB.expects(:get).with(:partners_exchange_token).returns('token123')
       api_stub = stub
       PartnersAPI.expects(:new).with(
         ctx: @context,
