0de55834b8268defa5252ed41c1acc43327da327
Melanie Wang <melanie.wang@shopify.com>
2021-03-17 14:18:44 -0400

    Update tests for AdminAPI changes

--- lib/shopify-cli/admin_api.rb
+++ lib/shopify-cli/admin_api.rb
@@ -88,9 +88,9 @@ module ShopifyCli
       def authenticated_req(ctx)
         yield
       rescue API::APIRequestUnauthorizedError
-        # authenticate(ctx)
-        # retry
-        ctx.debug("APIRequestUnauthorizedError!!")
+        authenticate(ctx)
+        retry
+        # ctx.debug("APIRequestUnauthorizedError!!")
       end
 
       def authenticate(ctx)
