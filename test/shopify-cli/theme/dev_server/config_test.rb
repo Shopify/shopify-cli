98bb4d428f8d0b708de7f2be7adc5328f968a002
Josh Larson <josh.larson@shopify.com>
2021-03-25 11:04:50 -0500

    Allow an environment to be specified

---
+++ test/shopify-cli/theme/dev_server/config_test.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+require "test_helper"
+require "shopify-cli/theme/dev_server"
+
+class ThemeTest < Minitest::Test
+  def test_uses_development_environment_by_default
+    config = ShopifyCli::Theme::DevServer::Config.from_path(ShopifyCli::ROOT + "/test/fixtures/theme")
+
+    assert_equal("123456789", config.theme_id)
+  end
+
+  def test_uses_another_environment_if_specified
+    config = ShopifyCli::Theme::DevServer::Config.from_path(ShopifyCli::ROOT + "/test/fixtures/theme", environment: "staging")
+
+    assert_equal("567891234", config.theme_id)
+  end
+end
