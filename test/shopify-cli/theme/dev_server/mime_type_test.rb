11c606a02ac55fdd625647fdb90bf36612ed7883
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-03-30 11:23:48 -0400

    Add class for detecting mime types in Theme::DevServer

---
+++ test/shopify-cli/theme/dev_server/mime_type_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+require "test_helper"
+require "shopify-cli/theme/dev_server"
+
+module ShopifyCli
+  module Theme
+    module DevServer
+      class MimeTypeTest < Minitest::Test
+        def test_by_filename
+          assert_equal("text/css", MimeType.by_filename("assets/style.css").name)
+          assert_equal("application/javascript", MimeType.by_filename("assets/site.js").name)
+          assert_equal("image/gif", MimeType.by_filename("assets/image.gif").name)
+          assert_equal("text/x-liquid", MimeType.by_filename("templates/template.liquid").name)
+          assert_equal("application/octet-stream", MimeType.by_filename("template.unknown").name)
+        end
+
+        def test_text
+          assert(MimeType.by_filename("assets/style.css").text?)
+          assert(MimeType.by_filename("assets/site.js").text?)
+          refute(MimeType.by_filename("assets/image.gif").text?)
+          assert(MimeType.by_filename("templates/template.liquid").text?)
+          refute(MimeType.by_filename("templates/template.unknown").text?)
+        end
+      end
+    end
+  end
+end
