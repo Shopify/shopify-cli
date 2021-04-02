dfe739c60118e921e2925d1280d65e5cc097d4d9
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-04-02 15:01:41 -0400

    Add test for dev server proxy requests coming from Core

--- test/shopify-cli/theme/dev_server/proxy_test.rb
+++ test/shopify-cli/theme/dev_server/proxy_test.rb
@@ -153,6 +153,33 @@ class ProxyTest < Minitest::Test
     assert_equal("PROXY RESPONSE", response.body)
   end
 
+  def test_do_not_pass_pending_files_to_core
+    ShopifyCli::DB
+      .stubs(:get)
+      .with(:shop)
+      .returns("dev-theme-server-store.myshopify.com")
+
+    ShopifyCli::DB
+      .stubs(:get)
+      .with(:storefront_renderer_production_exchange_token)
+      .returns("TOKEN")
+
+    # First request marks the endpoint as being served by Core
+    stub_request(:get, "https://dev-theme-server-store.myshopify.com/on-core?_fd=0&pb=0")
+      .to_return(status: 200, headers: {
+        # Doesn't have the x-storefront-renderer-rendered header
+      }).times(2)
+
+    stub_session_id_request
+    request.get("/on-core")
+
+    # Introduce pending files, but should not hit the POST endpoint
+    @theme.stubs(:pending_files).returns([
+      stub(relative_path: "layout/theme.liquid", read: "CONTENT"),
+    ])
+    request.get("/on-core")
+  end
+
   def test_replaces_secure_session_id_cookie
     stub_request(:get, "https://dev-theme-server-store.myshopify.com/?_fd=0&pb=0")
       .with(
