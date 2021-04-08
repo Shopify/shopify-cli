a603843c821987f16fa48d684d0bc91771eaf528
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-04-08 15:46:10 -0400

    Add theme check command
    Calling theme-check CLI interface

---
+++ test/project_types/theme/commands/check_test.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+require "project_types/theme/test_helper"
+require "theme_check"
+
+module Theme
+  module Commands
+    class CheckTest < MiniTest::Test
+      include TestHelpers::FakeUI
+
+      def test_command_runs_theme_check
+        context = ShopifyCli::Context.new
+        ThemeCheck::Cli.any_instance.expects(:run)
+
+        Theme::Command::Check.new(context).call([])
+      end
+
+      def test_parse_options
+        context = ShopifyCli::Context.new
+        ThemeCheck::Cli.any_instance.expects(:parse).with(["-l"])
+
+        command = Theme::Command::Check.new(context)
+        command.options.parse(nil, ["-l"])
+      end
+    end
+  end
+end
