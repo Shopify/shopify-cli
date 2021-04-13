dcc375843af2379e950f78b0c3ca2a19e1750ec0
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-04-13 16:31:51 -0400

    Move theme code not specific to dev server out of dev_server/ dir

--- test/shopify-cli/theme/dev_server/sse_test.rb
+++ test/shopify-cli/theme/dev_server/sse_test.rb
@@ -1,28 +1,34 @@
 # frozen_string_literal: true
 require "test_helper"
-require "shopify-cli/theme/dev_server"
+require "shopify-cli/theme/dev_server/sse"
 
-class SSETest < Minitest::Test
-  def test_broadcast_events
-    streams = ShopifyCli::Theme::DevServer::SSE::Streams.new
-    stream = streams.new
+module ShopifyCli
+  module Theme
+    module DevServer
+      class SSETest < Minitest::Test
+        def test_broadcast_events
+          streams = SSE::Streams.new
+          stream = streams.new
 
-    events = []
-    thread = Thread.new do
-      stream.each do |event|
-        events << event
+          events = []
+          thread = Thread.new do
+            stream.each do |event|
+              events << event
+            end
+          end
+
+          streams.broadcast("SOME-DATA")
+          streams.broadcast("SOME-MORE-DATA")
+          stream.close
+
+          thread.join
+
+          assert_equal([
+            "data: SOME-DATA\n\n\n",
+            "data: SOME-MORE-DATA\n\n\n",
+          ], events)
+        end
       end
     end
-
-    streams.broadcast("SOME-DATA")
-    streams.broadcast("SOME-MORE-DATA")
-    stream.close
-
-    thread.join
-
-    assert_equal([
-      "data: SOME-DATA\n\n\n",
-      "data: SOME-MORE-DATA\n\n\n",
-    ], events)
   end
 end
