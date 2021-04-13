32a63b088a26cd0798d242a7e99f5e54d3024332
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-04-13 16:31:51 -0400

    Move theme code not specific to dev server out of dev_server/ dir

--- test/shopify-cli/theme/dev_server/certificate_manager_test.rb
+++ test/shopify-cli/theme/dev_server/certificate_manager_test.rb
@@ -2,22 +2,28 @@
 require "test_helper"
 require "shopify-cli/theme/dev_server"
 
-class CertificateManagerTest < Minitest::Test
-  def test_find_or_create_certificates!
-    ctx = TestHelpers::FakeContext.new
-    domain_name = "test.shopify.local"
+module ShopifyCli
+  module Theme
+    module DevServer
+      class CertificateManagerTest < Minitest::Test
+        def test_find_or_create_certificates!
+          ctx = TestHelpers::FakeContext.new
+          domain_name = "test.shopify.local"
 
-    manager = ShopifyCli::Theme::DevServer::CertificateManager.new(ctx, domain_name)
+          manager = CertificateManager.new(ctx, domain_name)
 
-    manager.find_or_create_certificates!
+          manager.find_or_create_certificates!
 
-    private_key_file = OpenSSL::PKey::RSA.new(manager.private_key)
-    certificate_file = OpenSSL::X509::Certificate.new(manager.certificate)
+          private_key_file = OpenSSL::PKey::RSA.new(manager.private_key)
+          certificate_file = OpenSSL::X509::Certificate.new(manager.certificate)
 
-    expected_common_name = OpenSSL::X509::Name.new([["CN", domain_name]])
+          expected_common_name = OpenSSL::X509::Name.new([["CN", domain_name]])
 
-    assert(private_key_file.private?)
-    assert_equal(expected_common_name, certificate_file.subject)
-    assert_includes(certificate_file.extensions.map(&:to_s).flatten, "subjectAltName = DNS:test.shopify.local")
+          assert(private_key_file.private?)
+          assert_equal(expected_common_name, certificate_file.subject)
+          assert_includes(certificate_file.extensions.map(&:to_s).flatten, "subjectAltName = DNS:test.shopify.local")
+        end
+      end
+    end
   end
 end
