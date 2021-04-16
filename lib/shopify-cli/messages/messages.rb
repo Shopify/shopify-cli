1404177b07319a0777b4607e484ae93d41db7f4c
Melanie Wang <melanie.wang@shopify.com>
2021-04-16 16:04:38 -0400

    Ensure login for create commands

--- lib/shopify-cli/messages/messages.rb
+++ lib/shopify-cli/messages/messages.rb
@@ -186,6 +186,8 @@ module ShopifyCli
           },
         },
 
+        login_prompt: "Please ensure you've logged in with {{command:%s login}} and try again",
+
         populate: {
           help: <<~HELP,
             Populate your Shopify development store with example customers, orders, or products.
