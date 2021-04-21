c99e9c86ad850969e46ec829ca09121d087edf1a
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-04-21 13:41:32 -0400

    Move development theme logic under DevelopmentTheme subclass

--- lib/shopify-cli/theme/theme.rb
+++ lib/shopify-cli/theme/theme.rb
@@ -2,31 +2,20 @@
 require_relative "file"
 require_relative "ignore_filter"
 
-require "socket"
-require "securerandom"
-
 module ShopifyCli
   module Theme
     class Theme
-      # Files waiting to be uploaded to the Online Store
-      attr_reader :config
+      attr_reader :config, :id
 
-      def initialize(ctx, config)
+      def initialize(ctx, config, id: nil)
         @ctx = ctx
         @config = config
+        @id = id
         @ignore_filter = IgnoreFilter.new(root, patterns: config.ignore_files, files: config.ignores)
       end
 
       def root
         @config.root
-      end
-
-      def id
-        ShopifyCli::DB.get(:development_theme_id)
-      end
-
-      def name
-        ShopifyCli::DB.get(:development_theme_name) || generate_theme_name
       end
 
       def theme_files
@@ -76,66 +65,12 @@ module ShopifyCli
         @ignore_filter.match?(self[file].path.to_s)
       end
 
-      def ensure_development_theme_exists!
-        create_development_theme unless development_theme_exists?
-
-        @ctx.debug("Using temporary development theme: ##{id} #{name}")
-      end
-
       def editor_url
         "https://#{shop}/admin/themes/#{id}/editor"
       end
 
       def preview_url
         "https://#{shop}/?preview_theme_id=#{id}"
-      end
-
-      private
-
-      def development_theme_exists?
-        return false unless id
-
-        ShopifyCli::AdminAPI.rest_request(
-          @ctx,
-          shop: shop,
-          path: "themes/#{id}.json",
-          api_version: "unstable",
-        )
-      rescue ShopifyCli::API::APIRequestNotFoundError
-        false
-      end
-
-      def create_development_theme
-        _status, body = ShopifyCli::AdminAPI.rest_request(
-          @ctx,
-          shop: shop,
-          path: "themes.json",
-          body: JSON.generate({
-            theme: {
-              name: name,
-              role: "development",
-            },
-          }),
-          method: "POST",
-          api_version: "unstable",
-        )
-
-        theme_id = body["theme"]["id"]
-
-        @ctx.debug("Created temporary development theme: #{theme_id}")
-
-        ShopifyCli::DB.set(development_theme_id: theme_id)
-      end
-
-      def generate_theme_name
-        hostname = Socket.gethostname.split(".").shift
-        hash = SecureRandom.hex(3)
-
-        theme_name = "Development (#{hash}-#{hostname})"
-
-        ShopifyCli::DB.set(development_theme_name: theme_name)
-
-        theme_name
       end
     end
   end
