f54dcbe33527a57f3c3bbe33e087e50d9eccd8f5
Jumoke Fajinmi <jumoke.fajinmi@shopify.com>
2021-04-27 13:50:41 -0400

    update extension command and register tests.

--- test/project_types/extension/commands/extension_command_test.rb
+++ test/project_types/extension/commands/extension_command_test.rb
@@ -5,33 +5,29 @@ require "project_types/extension/extension_test_helpers"
 module Extension
   module Commands
     class ExtensionCommandTest < MiniTest::Test
-      include TestHelpers::FakeUI
       include ExtensionTestHelpers::Messages
-      include ExtensionTestHelpers::TempProjectSetup
 
       def setup
         super
         ShopifyCli::ProjectType.load_type(:extension)
+        @project = ExtensionTestHelpers.fake_extension_project(with_mocks: true)
         @command = ExtensionCommand.new
       end
 
       def test_project_returns_the_current_extension_project
-        setup_temp_project
-
         assert_equal @project, @command.project
       end
 
       def test_project_memoizes_the_current_extension_project
-        setup_temp_project
         ExtensionProject.expects(:current).returns(@project).once
 
         @command.project
         @command.project
       end
 
-      def test_extension_type_aborts_if_a_lazily_initialized_field_of_an_unknown_type_is_accessed
+      def test_extension_command_aborts_if_a_lazily_initialized_field_of_an_unknown_type_is_accessed
         unknown_type = "unknown_type"
-        setup_temp_project(type_identifier: unknown_type)
+        project = ExtensionTestHelpers.fake_extension_project(with_mocks: true, type_identifier: unknown_type)
 
         io = capture_io_and_assert_raises(ShopifyCli::Abort) { @command.specification_handler.features }
 
@@ -40,21 +36,16 @@ module Extension
         ])
       end
 
-      def test_extension_type_returns_a_lazy_specification_handler
-        setup_temp_project
-
+      def test_extension_command_returns_a_lazy_specification_handler
         assert_kind_of(Models::LazySpecificationHandler, @command.specification_handler)
       end
 
-      def test_extension_type_memoizes_the_extension_type
-        setup_temp_project
-
+      def test_extension_command_memoizes_the_extension_type
         @command.specification_handler.specification
         @command.specification_handler.specification
       end
 
       def test_accessing_the_extension_type_identifier_does_not_result_in_fetching_specifications
-        setup_temp_project
         @command.specification_handler.identifier
       end
     end
