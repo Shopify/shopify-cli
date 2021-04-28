c611ed5708a08e642fbe04b2b2a17a3413703a7f
Paulo Margarido <paulo.margarido@shopify.com>
2021-04-28 15:39:13 -0400

    Adding PHP project type

---
+++ lib/project_types/php/cli.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+module PHP
+  class Project < ShopifyCli::ProjectType
+    title("PHP App")
+    creator("PHP::Commands::Create")
+    # connector("PHP::Commands::Connect")
+
+    # register_command("Node::Commands::Deploy", "deploy")
+
+    require Project.project_filepath("messages/messages")
+    register_messages(PHP::Messages::MESSAGES)
+  end
+
+  # define/autoload project specific Commands
+  module Commands
+    autoload :Create, Project.project_filepath("commands/create")
+  end
+
+  # define/autoload project specific Tasks
+  module Tasks
+  end
+
+  # define/autoload project specific Forms
+  module Forms
+    # autoload :Create, Project.project_filepath("forms/create")
+  end
+end
