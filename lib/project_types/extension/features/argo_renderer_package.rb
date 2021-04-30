ab4acd430e0fb3ddf4cef25cfef8547c4725a52d
Konstantin Tennhard <konstantin.tennhard@shopify.com>
2021-04-30 18:24:19 -0400

    Move parsing code into ArgoRendererPackage

--- lib/project_types/extension/features/argo_renderer_package.rb
+++ lib/project_types/extension/features/argo_renderer_package.rb
@@ -5,15 +5,26 @@ module Extension
 
       ARGO_CHECKOUT = "@shopify/argo-checkout"
       ARGO_ADMIN = "@shopify/argo-admin"
+      ARGO_POST_PURCHASE = "@shopify/argo-post-purchase"
 
       PACKAGE_NAMES = [
         ARGO_CHECKOUT,
         ARGO_ADMIN,
+        ARGO_POST_PURCHASE
       ].freeze
       MINIMUM_ARGO_VERSION = "0.9.3".freeze
 
       property! :package_name, accepts: PACKAGE_NAMES
       property! :version, accepts: String
+
+      class << self
+        def from_package_manager(package_manager_output)
+          pattern = /(?<name>#{PACKAGE_NAMES.join("|")})@(?<version>\d.*)$/
+          match = package_manager_output.match(pattern)
+          raise PackageNotFound, package_manager_output if match.nil?
+          return new(package_name: match[:name], version: match[:version].strip)
+        end
+      end
 
       def checkout?
         package_name == ARGO_CHECKOUT
--- test/project_types/extension/extension_test_helpers/dummy_argo.rb
+++ test/project_types/extension/extension_test_helpers/dummy_argo.rb
@@ -2,8 +2,10 @@ module Extension
   module ExtensionTestHelpers
     class DummyArgo < Extension::Features::Argo
       GIT_TEMPLATE = "https://something"
-      RENDERER_PACKAGE = "@test-renderer-package"
+      RENDERER_PACKAGE = "@shopify/argo-admin"
       private_constant :GIT_TEMPLATE, :RENDERER_PACKAGE
+
+      property :fake_renderer_package, accepts: Features::ArgoRendererPackage
 
       def git_template
         GIT_TEMPLATE
@@ -11,6 +13,17 @@ module Extension
 
       def renderer_package_name
         RENDERER_PACKAGE
+      end
+
+      def renderer_version=(renderer_version)
+        self.fake_renderer_package = Features::ArgoRendererPackage.new(
+          package_name: renderer_package_name,
+          version: renderer_version
+        )
+      end
+
+      def renderer_package(context)
+        fake_renderer_package || super(context)
       end
     end
   end
