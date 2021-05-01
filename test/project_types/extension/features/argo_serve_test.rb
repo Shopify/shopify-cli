9195ff7892b68c427d05ed944de0a7362f68b6f7
Kevin O'Sullivan <mkevin.osullivan@shopify.com>
2021-05-01 11:51:21 -0400

    fix tests, linting

--- test/project_types/extension/features/argo_serve_test.rb
+++ test/project_types/extension/features/argo_serve_test.rb
@@ -121,8 +121,7 @@ renderer_package_name: "@shopify/argo-checkout")
 renderer_package_name: "@shopify/argo-admin")
       end
 
-      def stub_argo_enabled_shop(api_key: @api_key, registration_uuid: @registration_uuid, argo_version: @argo_version)
-        _ = argo_version
+      def stub_argo_enabled_shop(api_key: @api_key, registration_uuid: @registration_uuid, _argo_version: @argo_version)
         ShopifyCli::Shopifolk.stubs(:check).returns(true)
         ShopifyCli::Feature.stubs(:enabled?).with(:argo_admin_beta).returns(true)
         ShopifyCli::Tasks::EnsureEnv.stubs(:call)
