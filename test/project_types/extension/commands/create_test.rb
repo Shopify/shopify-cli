9a2532420966b5e13e6f9b18af6e08def63e2964
Jumoke Fajinmi <jumoke.fajinmi@shopify.com>
2021-05-04 14:58:53 -0400

    refactor create test

--- test/project_types/extension/commands/create_test.rb
+++ test/project_types/extension/commands/create_test.rb
@@ -5,17 +5,18 @@ require "project_types/extension/extension_test_helpers"
 module Extension
   module Commands
     class CreateTest < MiniTest::Test
-      include TestHelpers::FakeUI
-      include ExtensionTestHelpers
-      include ExtensionTestHelpers::TestExtensionSetup
       include ExtensionTestHelpers::Messages
-      include ExtensionTestHelpers::Stubs::GetOrganizations
       include ExtensionTestHelpers::Stubs::GetApp
 
       def setup
         super
         @name = "My Ext"
         @directory_name = "my_ext"
+
+        ShopifyCli::ProjectType.load_type(:extension)
+
+        ExtensionTestHelpers.fake_extension_project(with_mocks: true)
+        @specification_handler = ExtensionTestHelpers.test_specification_handler
 
         @app = Models::App.new(title: "Fake", api_key: "1234", secret: "4567", business_name: "Fake Business")
         stub_get_app(api_key: "1234", app: @app)
@@ -31,7 +32,8 @@ module Extension
         Models::SpecificationHandlers::Default.any_instance.expects(:create).never
 
         io = capture_io_and_assert_raises(ShopifyCli::Abort) do
-          run_create(%W(extension --name=#{@name} --type=#{@test_extension_type.identifier} --api-key=#{@app.api_key}))
+          run_create(%W(extension --name=#{@name} --type=#{@specification_handler.identifier}
+                        --api-key=#{@app.api_key}))
         end
 
         assert_message_output(io: io, expected_content: [
@@ -43,19 +45,20 @@ module Extension
         Dir.expects(:exist?).with(@directory_name).returns(false).once
         Models::SpecificationHandlers::Default
           .any_instance.expects(:create).with(@directory_name, @context).returns(true).once
-        ExtensionProject.expects(:write_cli_file).with(context: @context, type: @test_extension_type.identifier).once
+        ExtensionProject.expects(:write_cli_file).with(context: @context, type: @specification_handler.identifier).once
         ExtensionProject
           .expects(:write_env_file)
           .with(context: @context, title: @name, api_key: @app.api_key, api_secret: @app.secret)
           .once
 
         io = capture_io do
-          run_create(%W(extension --name=#{@name} --type=#{@test_extension_type.identifier} --api-key=#{@app.api_key}))
+          run_create(%W(extension --name=#{@name} --type=#{@specification_handler.identifier}
+                        --api-key=#{@app.api_key}))
         end
 
         assert_message_output(io: io, expected_content: [
           @context.message("create.ready_to_start", @directory_name, @name),
-          @context.message("create.learn_more", @test_extension_type.name),
+          @context.message("create.learn_more", @specification_handler.name),
         ])
       end
 
@@ -67,7 +70,8 @@ module Extension
         ExtensionProject.expects(:write_env_file).never
 
         io = capture_io do
-          run_create(%W(extension --name=#{@name} --type=#{@test_extension_type.identifier} --api-key=#{@app.api_key}))
+          run_create(%W(extension --name=#{@name} --type=#{@specification_handler.identifier}
+                        --api-key=#{@app.api_key}))
         end
 
         assert_message_output(io: io, expected_content: @context.message("create.try_again"))
@@ -85,10 +89,7 @@ module Extension
       private
 
       def run_create(arguments)
-        specifications = DummySpecifications.build(
-          custom_handler_root: File.expand_path("../../", __FILE__),
-          custom_handler_namespace: ::Extension::ExtensionTestHelpers,
-        )
+        specifications = ExtensionTestHelpers.test_specifications
         Models::Specifications.stubs(:new).returns(specifications)
         Commands::Create.ctx = @context
         Commands::Create.call(arguments, "create", "create")
