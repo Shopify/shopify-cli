5d61d8696e94078a67dd897fec8e6c68d401c7c1
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-05-05 14:31:24 -0400

    Add theme init command that clones Dawn
    Fixes #84

---
+++ test/project_types/theme/commands/init_test.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+require "project_types/theme/test_helper"
+
+module Theme
+  module Commands
+    class InitTest < MiniTest::Test
+      include TestHelpers::FakeUI
+
+      def setup
+        super
+
+        @ctx = ShopifyCli::Context.new
+        @command = Theme::Command::Init.new(@ctx)
+      end
+
+      def test_clones_repo
+        ShopifyCli::Git.expects(:clone).with(Theme::Command::Init::DEFAULT_CLONE_URL, "repo-name")
+
+        @command.call(["repo-name"], "init")
+      end
+
+      def test_ask_repo_name
+        ShopifyCli::Git.expects(:clone).with(Theme::Command::Init::DEFAULT_CLONE_URL, "repo-name")
+        CLI::UI::Prompt.expects(:ask).returns("repo-name")
+
+        @command.call([], "init")
+      end
+
+      def test_repo_url_in_options
+        ShopifyCli::Git.expects(:clone).with("CLONE_URL", "repo-name")
+
+        @command.options.flags[:clone_url] = "CLONE_URL"
+        @command.call(["repo-name"], "init")
+      end
+    end
+  end
+end
