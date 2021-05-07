f455db4e8337bd01fb5b071c5ee95a3464c5ba73
Melanie Wang <melanie.wang@shopify.com>
2021-05-07 09:34:21 -0400

    Update lib/shopify-cli/messages/messages.rb
    Way better wording!
    
    Co-authored-by: shainaraskas <58563081+shainaraskas@users.noreply.github.com>

--- lib/shopify-cli/messages/messages.rb
+++ lib/shopify-cli/messages/messages.rb
@@ -144,7 +144,7 @@ module ShopifyCli
             timeout: "Timed out while waiting for response from Shopify",
             local_identity_not_running: "Identity needs to be running locally in order to proceed.",
             reauthenticate: "Please login again with {{command:shopify login}}",
-            invalid_destination: "Invalid shop value %s, please logout and try again",
+            invalid_destination: "The store %s doesn't exist. Please log out and try again.",
           },
 
           location: {
