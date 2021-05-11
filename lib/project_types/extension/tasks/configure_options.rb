958ef061a76b89e37ae849934ca610ddaebc2771
Chris AtLee <chris@atlee.ca>
2021-05-11 16:02:58 -0400

    Rename to Online Store Theme Extensions to match name in core

--- lib/project_types/extension/tasks/configure_options.rb
+++ lib/project_types/extension/tasks/configure_options.rb
@@ -13,7 +13,7 @@ module Extension
       private
 
       def configure_skip_build(attributes)
-        attributes[:options].merge!(skip_build: attributes[:identifier] == "online_store_app_extension")
+        attributes[:options].merge!(skip_build: attributes[:identifier] == "online_store_theme_extension")
       end
     end
   end
