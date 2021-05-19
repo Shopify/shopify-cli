67744bac7db0f2ffa572bb3a0eded1ffb5c9321d
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-05-19 10:50:55 -0400

    Fail silently when trying to delete dev theme on logout

--- lib/shopify-cli/commands/logout.rb
+++ lib/shopify-cli/commands/logout.rb
@@ -5,7 +5,7 @@ module ShopifyCli
   module Commands
     class Logout < ShopifyCli::Command
       def call(*)
-        ShopifyCli::Theme::DevelopmentTheme.delete(@ctx)
+        try_delete_development_theme
         ShopifyCli::IdentityAuth.delete_tokens_and_keys
         ShopifyCli::DB.del(:shop) if ShopifyCli::DB.exists?(:shop)
         ShopifyCli::Shopifolk.reset
@@ -14,6 +14,14 @@ module ShopifyCli
 
       def self.help
         ShopifyCli::Context.message("core.logout.help", ShopifyCli::TOOL_NAME)
+      end
+
+      private
+
+      def try_delete_development_theme
+        ShopifyCli::Theme::DevelopmentTheme.delete(@ctx)
+      rescue ShopifyCli::API::APIRequestError
+        # Ignore since we can't delete it
       end
     end
   end
