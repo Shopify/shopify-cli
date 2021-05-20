4920e09f02fcd93e18fd80224b0be397d76e0902
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-05-20 12:08:33 -0400

    Update theme push command to new IgnoreFilter & API changes

--- lib/project_types/theme/forms/select.rb
+++ lib/project_types/theme/forms/select.rb
@@ -2,11 +2,11 @@ module Theme
   module Forms
     class Select < ShopifyCli::Form
       attr_accessor :theme
-      flag_arguments :config, :title, :exclude_roles
+      flag_arguments :root, :title, :exclude_roles
 
       def ask
         self.theme = CLI::UI::Prompt.ask(title, allow_empty: false) do |handler|
-          ShopifyCli::Theme::Theme.all(@ctx, config).each do |theme|
+          ShopifyCli::Theme::Theme.all(@ctx, root: root).each do |theme|
             next if exclude_roles&.include?(theme.role)
             handler.option("#{theme.name} {{green:[#{theme.role}]}}") { theme }
           end
