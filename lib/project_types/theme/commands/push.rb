745e27b2df02d1b456e78cdbc2c01e5c6e107997
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-05-20 12:08:33 -0400

    Update theme push command to new IgnoreFilter & API changes

--- lib/project_types/theme/commands/push.rb
+++ lib/project_types/theme/commands/push.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 require "shopify-cli/theme/theme"
 require "shopify-cli/theme/development_theme"
+require "shopify-cli/theme/ignore_filter"
 require "shopify-cli/theme/uploader"
 
 module Theme
@@ -14,26 +15,21 @@ module Theme
         parser.on("-j", "--json") { flags[:json] = true }
         parser.on("-a", "--allow-live") { flags[:allow_live] = true }
         parser.on("-p", "--publish") { flags[:publish] = true }
-
-        # Only used for the config.yml, can be removed once usage is gone
-        parser.on("--env=ENV") { |env| flags[:env] = env }
       end
 
       def call(args, _name)
         root = args.first || "."
-        environment = options.flags[:env] || "development"
-        config = ShopifyCli::Theme::Config.from_path(root, environment: environment)
         delete = !options.flags[:nodelete]
 
         theme = if (theme_id = options.flags[:theme_id])
-          ShopifyCli::Theme::Theme.new(@ctx, config, id: theme_id)
+          ShopifyCli::Theme::Theme.new(@ctx, root: root, id: theme_id)
         elsif options.flags[:development]
-          theme = ShopifyCli::Theme::DevelopmentTheme.new(@ctx, config)
+          theme = ShopifyCli::Theme::DevelopmentTheme.new(@ctx, root: root)
           theme.ensure_exists!
           theme
         elsif options.flags[:unpublished]
           name = CLI::UI::Prompt.ask(@ctx.message("theme.push.name"), allow_empty: false)
-          theme = ShopifyCli::Theme::Theme.new(@ctx, config, name: name, role: "unpublished")
+          theme = ShopifyCli::Theme::Theme.new(@ctx, root: root, name: name, role: "unpublished")
           theme.create
           theme
         else
@@ -41,7 +37,7 @@ module Theme
             @ctx,
             [],
             title: @ctx.message("theme.push.select"),
-            config: config,
+            root: root,
           ).theme
         end
 
@@ -49,7 +45,8 @@ module Theme
           return unless CLI::UI::Prompt.confirm(@ctx.message("theme.push.live"))
         end
 
-        uploader = ShopifyCli::Theme::Uploader.new(@ctx, theme)
+        ignore_filter = ShopifyCli::Theme::IgnoreFilter.from_path(root)
+        uploader = ShopifyCli::Theme::Uploader.new(@ctx, theme: theme, ignore_filter: ignore_filter)
         begin
           uploader.start_threads
           if options.flags[:json]
