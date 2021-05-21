119c2e3903fc937cef879b93082c3e51d5858880
Marc-André Cournoyer <marcandre.cournoyer@shopify.com>
2021-05-21 13:31:54 -0400

    Order themes live,unpublished,development,demo when listing
    Add color for each role, and tag your current dev theme.
    
    Also, do not list other people dev themes by default. Overridable for delete w/ --show-all option.

--- lib/project_types/theme/forms/select.rb
+++ lib/project_types/theme/forms/select.rb
@@ -2,15 +2,57 @@ module Theme
   module Forms
     class Select < ShopifyCli::Form
       attr_accessor :theme
-      flag_arguments :root, :title, :exclude_roles
+      flag_arguments :root, :title, :exclude_roles, :include_foreign_developments
 
       def ask
         self.theme = CLI::UI::Prompt.ask(title, allow_empty: false) do |handler|
-          ShopifyCli::Theme::Theme.all(@ctx, root: root).each do |theme|
+          themes.each do |theme|
             next if exclude_roles&.include?(theme.role)
-            handler.option("#{theme.name} {{green:[#{theme.role}]}}") { theme }
+            next if !include_foreign_developments && theme.foreign_development?
+            handler.option("#{theme.name} #{theme_tags(theme)}") { theme }
           end
         end
+      end
+
+      private
+
+      def themes
+        @themes ||= ShopifyCli::Theme::Theme.all(@ctx, root: root)
+          .sort_by { |theme| theme_sort_order(theme) }
+      end
+
+      def theme_sort_order(theme)
+        case theme.role
+        when "live"
+          0
+        when "unpublished"
+          1
+        when "development"
+          2
+        else
+          3
+        end
+      end
+
+      def theme_tags(theme)
+        color = case theme.role
+        when "live"
+          "green"
+        when "unpublished"
+          "yellow"
+        when "development"
+          "blue"
+        else
+          "grey"
+        end
+
+        tags = ["{{#{color}:[#{theme.role}]}}"]
+
+        if theme.current_development?
+          tags << "{{cyan:[yours]}}}}"
+        end
+
+        tags.join(" ")
       end
     end
   end
