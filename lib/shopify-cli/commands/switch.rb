933ac44a501b8da914255ec0e1935662ff336300
Melanie Wang <melanie.wang@shopify.com>
2021-05-21 14:14:12 -0400

    Enable shop flag

--- lib/shopify-cli/commands/switch.rb
+++ lib/shopify-cli/commands/switch.rb
@@ -3,13 +3,24 @@ require "shopify_cli"
 module ShopifyCli
   module Commands
     class Switch < ShopifyCli::Command
+      options do |parser, flags|
+        parser.on("--shop=SHOP") do |shop|
+          flags[:shop] = shop
+        end
+      end
+
       def call(*)
-        AdminAPI.get_shop_or_abort(@ctx)
-        res = ShopifyCli::Tasks::SelectOrgAndShop.call(@ctx)
-        DB.set(shop: res[:shop_domain])
+        shop = if options.flags[:shop]
+          Login.new.validate_shop(options.flags[:shop])
+        else
+          AdminAPI.get_shop_or_abort(@ctx)
+          res = ShopifyCli::Tasks::SelectOrgAndShop.call(@ctx)
+          res[:shop_domain]
+        end
+        DB.set(shop: shop)
         IdentityAuth.new(ctx: @ctx).reauthenticate
 
-        @ctx.puts(@ctx.message("core.switch.success", res[:shop_domain]))
+        @ctx.puts(@ctx.message("core.switch.success", shop))
       end
 
       def self.help
