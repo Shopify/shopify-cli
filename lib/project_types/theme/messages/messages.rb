2339511995712d13dbbf6c1d069b45953b5402d4
Owen Williams <owen@char.gd>
2021-05-21 14:49:42 -0400

    Clarify serve

--- lib/project_types/theme/messages/messages.rb
+++ lib/project_types/theme/messages/messages.rb
@@ -85,7 +85,7 @@ module Theme
         },
         serve: {
           help: <<~HELP,
-            Uploads the current theme as a development theme to the store that you're connected to, and opens it in your default browser. While running, changes will push to the store in real time. Also prints the connected store URL to your terminal.
+            Uploads the current theme as a development theme to the connected store, then prints theme editor and preview URLs to your terminal. While running, changes will push to the store in real time. 
             Usage: {{command:%s theme serve}}
           HELP
           serve: "Viewing theme...",
