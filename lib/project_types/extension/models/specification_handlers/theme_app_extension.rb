597cc827cf23a1342d877b5e3e503e1e0bb7ae96
Chris AtLee <chris@atlee.ca>
2021-05-27 16:13:11 -0400

    Create sample files/directories when creating a new theme extension
    Fixes #158

--- lib/project_types/extension/models/specification_handlers/theme_app_extension.rb
+++ lib/project_types/extension/models/specification_handlers/theme_app_extension.rb
@@ -5,11 +5,41 @@ require "json"
 module Extension
   module Models
     module SpecificationHandlers
+      APP_BLOCK_SAMPLE = <<~BLOCK
+        Your theme app extension's liquid goes here.
+
+        {% schema %}
+        {
+          "name": "Sample App Block",
+          "target": "section"
+        }
+        {% endschema %}
+      BLOCK
+
+      APP_EMBED_BLOCK_SAMPLE = <<~BLOCK
+        /*
+        Your theme app extension's liquid goes here and will be rendered inside
+        the <head> section of product pages.
+        */
+
+        {% schema %}
+        {
+          "name": "Sample App Embed Block",
+          "target": "head",
+          "templates": ["product"]
+        }
+        {% endschema %}
+      BLOCK
+
       class ThemeAppExtension < Default
         SUPPORTED_BUCKETS = %w(assets blocks snippets locales)
+
         def create(directory_name, context)
           context.root = File.join(context.root, directory_name)
-          FileUtils.mkdir_p(context.root)
+
+          FileUtils.makedirs(SUPPORTED_BUCKETS.map { |b| File.join(context.root, b) })
+          File.write(File.join(context.root, "blocks", "app_block_sample.liquid"), APP_BLOCK_SAMPLE)
+          File.write(File.join(context.root, "blocks", "app_embed_block_sample.liquid"), APP_EMBED_BLOCK_SAMPLE)
         end
 
         def config(context)
