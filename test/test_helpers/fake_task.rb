189267a391d694d8795832d800d4f7a83c89a794
Jacob Steves <jacob.steves@shopify.com>
2021-06-17 17:30:52 -0400

    support passing in arguments with prerequisite_task

--- test/test_helpers/fake_task.rb
+++ test/test_helpers/fake_task.rb
@@ -6,9 +6,16 @@ module TestHelpers
       end
     end
 
+    class FakeTaskWithArgs < ShopifyCli::Task
+      def call(ctx, *args)
+        ctx.puts("success with args #{args.join}!")
+      end
+    end
+
     def setup
       @registry = ShopifyCli::Tasks::TaskRegistry.new
       @registry.add(FakeTask, :fake)
+      @registry.add(FakeTaskWithArgs, :fake_with_args)
       super
     end
 
