4473bd6d1fc8066f938a064127fe8a8ffb0d5b3d
Duncan Uszkay <duncan.uszkay@shopify.com>
2021-06-21 17:45:11 -0400

    Tentatively changing the cert store for http requests to match the system

--- lib/shopify-cli/http_request.rb
+++ lib/shopify-cli/http_request.rb
@@ -14,8 +14,12 @@ module ShopifyCli
       end
 
       def request(uri, body, headers, req)
+        cert_store = OpenSSL::X509::Store.new
+        cert_store.set_default_paths
+
         http = ::Net::HTTP.new(uri.host, uri.port)
         http.use_ssl = true
+        http.cert_store = cert_store
 
         req.body = body unless body.nil?
         req["Content-Type"] = "application/json"
