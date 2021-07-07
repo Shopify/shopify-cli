c6826d7211e90cceeb5ae317157f03c3971573ee
Alex Bradley <alexandre.bradley@shopify.com>
2021-07-07 14:20:44 -0400

    rework test cases

--- test/project_types/script/layers/infrastructure/languages/assemblyscript_project_creator_test.rb
+++ test/project_types/script/layers/infrastructure/languages/assemblyscript_project_creator_test.rb
@@ -6,6 +6,12 @@ describe Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
   include TestHelpers::FakeFS
 
   let(:context) { TestHelpers::FakeContext.new }
+  let(:extension_point_type) { "payment_methods" }
+  let(:extension_point) { Script::Layers::Domain::ExtensionPoint.new(extension_point_type, extension_point_config) }
+  let(:project_creator) do
+    Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
+      .new(ctx: context, extension_point: extension_point, script_name: script_name, path_to_project: script_name)
+  end
   let(:fake_capture2e_response) { [nil, OpenStruct.new(success?: true)] }
 
   let(:extension_point_type) { "payment_methods" }
@@ -43,15 +49,41 @@ describe Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
     context.mkdir_p(project_name)
   end
 
+  def system_output(msg:, success:)
+    [msg, OpenStruct.new(success?: success)]
+  end
+
   describe ".setup_dependencies" do
     subject { project_creator.setup_dependencies }
 
-    it "should setup dependencies" do
-      Script::Layers::Infrastructure::Languages::ProjectCreator.any_instance
-        .expects(:setup_dependencies)
-        .with
+    it "should setup sparse checkout" do
+      # sparse checkout
+      context
+        .expects(:capture2e)
+        .with("git init").once
+        .returns(system_output(msg: "", success: true))
+      context
+        .expects(:capture2e)
+        .with("git remote add -f origin #{extension_point.sdks.assemblyscript.repo}")
         .once
+        .returns(system_output(msg: "", success: true))
+      context
+        .expects(:capture2e)
+        .with("git config core.sparsecheckout true")
+        .once
+        .returns(system_output(msg: "", success: true))
+      context
+        .expects(:capture2e)
+        .with("git sparse-checkout set #{project_creator.sparse_checkout_set_path}")
+        .once
+        .returns(system_output(msg: "", success: true))
+      context
+        .expects(:capture2e)
+        .with("git pull origin master")
+        .once
+        .returns(system_output(msg: "", success: true))
 
+      # npmrc generation
       context
         .expects(:capture2e)
         .with(Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator::NPM_SET_REGISTRY_COMMAND)
@@ -60,6 +92,104 @@ describe Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
         .expects(:capture2e)
         .with(Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator::NPM_SET_ENGINE_STRICT_COMMAND)
         .returns(fake_capture2e_response)
+
+      # package.json generation
+      context
+        .expects(:capture2e)
+        .with("npm show @shopify/extension-point-as-fake version --json")
+        .once
+        .returns([JSON.generate("2.0.0"), OpenStruct.new(success?: true)])
+
+      # moving files up to project root
+      type = extension_point.type
+      source = File.join(project_creator.path_to_project, project_creator.sparse_checkout_set_path)
+      FileUtils.expects(:copy_entry).with(source, project_creator.path_to_project)
+
+      # confirm package.json - only once this file is checked into the repo
+      # File.expects(:read).with("package.json").returns("name = payment-filter-default")
+      # File.expects(:write).with("package.json", "name = #{script_name}")
+
+      # clean-up git files
+      context.expects(:rm_rf).with(".git")
+      context.expects(:rm_rf).with("packages")
+
+      subject
+    end
+
+    it "should raise a service failure error if the git repository cannot be iniitialized" do
+      context
+        .expects(:capture2e)
+        .with("git init").once
+        .returns(system_output(msg: "Couldn't initialize git repository", success: false))
+
+      assert_raises(Script::Layers::Infrastructure::Errors::SystemCallFailureError) { subject }
+    end
+
+    it "should raise a service failure error if the git remote cannot be configured" do
+      context.expects(:capture2e).once.returns(system_output(msg: "", success: true))
+
+      context
+        .expects(:capture2e)
+        .with("git remote add -f origin #{extension_point.sdks.assemblyscript.repo}").once
+        .returns(system_output(msg: "Couldn't set remote origin", success: false))
+
+      assert_raises(Script::Layers::Infrastructure::Errors::SystemCallFailureError) { subject }
+    end
+
+    it "should raise a service failure error if sparse checkout cannot be enabled" do
+      context.expects(:capture2e).twice.returns(system_output(msg: "", success: true))
+
+      context
+        .expects(:capture2e)
+        .with("git config core.sparsecheckout true").once
+        .returns(system_output(msg: "Couldn't enable sparse checkout", success: false))
+
+      assert_raises(Script::Layers::Infrastructure::Errors::SystemCallFailureError) { subject }
+    end
+
+    it "should raise a service failure error if sparse checkout cannot be set" do
+      context.expects(:capture2e).times(3).returns(system_output(msg: "", success: true))
+
+      context
+        .expects(:capture2e)
+        .with("git sparse-checkout set #{project_creator.sparse_checkout_set_path}").once
+        .returns(system_output(msg: "Couldn't set sparse checkout", success: false))
+
+      assert_raises(Script::Layers::Infrastructure::Errors::SystemCallFailureError) { subject }
+    end
+
+    it "raises if there is an error pulling from the remote's origin" do
+      context.expects(:capture2e).times(4).returns(system_output(msg: "", success: true))
+      context
+        .expects(:capture2e)
+        .with("git pull origin master")
+        .once
+        .returns(system_output(msg: "Fatal", success: false))
+      assert_raises(Script::Layers::Infrastructure::Errors::SystemCallFailureError) { subject }
+    end
+  end
+
+  describe "dependencies for extension points without SDK" do
+    subject { project_creator.setup_dependencies }
+
+    let(:extension_point) do
+      Script::Layers::Domain::ExtensionPoint.new(extension_point_type, extension_point_config)
+    end
+    let(:extension_point_config) do
+      {
+        "assemblyscript" => {
+          "package": "@shopify/extension-point-as-fake",
+          "toolchain-version": "*",
+        },
+      }
+    end
+
+    it "does not include SDK in package.json" do
+      context.expects(:capture2e).returns([JSON.generate("2.0.0"), OpenStruct.new(success?: true)]).times(3)
+      context.expects(:write).with do |_file, contents|
+        payload = JSON.parse(contents)
+        payload.dig("devDependencies", "@shopify/scripts-sdk-as").nil?
+      end
 
       subject
     end
