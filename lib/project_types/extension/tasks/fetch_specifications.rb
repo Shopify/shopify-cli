d7f026b2fbc0aadf49150816bd34e223366b0810
Chris AtLee <chris.atlee@shopify.com>
2021-07-22 18:16:40 -0400

    WIP - Cache specifications by api key

--- lib/project_types/extension/tasks/fetch_specifications.rb
+++ lib/project_types/extension/tasks/fetch_specifications.rb
@@ -7,11 +7,38 @@ module Extension
       property :api_key
 
       def call
-        response = ShopifyCli::PartnersAPI
-          .query(context, "fetch_specifications", api_key: api_key)
-          .dig("data", "extensionSpecifications")
-        context.abort(context.message("tasks.errors.parse_error")) if response.nil?
-        response
+        # TODO: Need a way to disable this for e.g. extension create
+        cache(api_key) do
+          response = ShopifyCli::PartnersAPI
+            .query(context, "fetch_specifications", api_key: api_key)
+            .dig("data", "extensionSpecifications")
+          context.abort(context.message("tasks.errors.parse_error")) if response.nil?
+          response
+        end
+      end
+
+      private
+
+      def cache(api_key)
+        specifications_dir = File.join(ShopifyCli.cache_dir, "specifications")
+        FileUtils.mkdir_p(specifications_dir)
+        filename = File.join(specifications_dir, "#{api_key}.json")
+
+        begin
+          if File.file?(filename) && File.mtime(filename) > Time.now - 86400
+            # Load from the cache
+            context.debug("cache hit: loading from #{filename}")
+            return JSON.parse(File.read(filename))
+          end
+        rescue JSON::JSONError
+        end
+
+        context.debug("cache miss: fetching specifications")
+        result = yield
+
+        context.debug("cache miss: writing to #{filename}")
+        File.write(filename, JSON.dump(result))
+        result
       end
     end
   end
