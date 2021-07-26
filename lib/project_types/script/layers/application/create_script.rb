bcbe588b7c451cd046550e49a8ab8bc784874c08
Alex Bradley <alexandre.bradley@shopify.com>
2021-07-26 16:40:17 -0400

    Refactoring ProjectCreator 🔥

--- lib/project_types/script/layers/application/create_script.rb
+++ lib/project_types/script/layers/application/create_script.rb
@@ -18,8 +18,22 @@ module Script
                 language: language,
                 no_config_ui: no_config_ui
               )
+
+              # the default value of this should be master
+              branch = "master"
+              # and be overwritten with a CLI argument - for now we'll just overwrite it here during dev
+              branch = "add-package-json"
+
               project_creator = Infrastructure::Languages::ProjectCreator
-                .for(ctx, language, extension_point, script_name, project.id)
+                .for(
+                  ctx, 
+                  language, 
+                  extension_point, 
+                  script_name, 
+                  project.id, 
+                  branch
+                  )
+
               install_dependencies(ctx, language, script_name, project_creator)
               bootstrap(ctx, project_creator)
               project
@@ -36,7 +50,7 @@ module Script
 
           def bootstrap(ctx, project_creator)
             UI::StrictSpinner.spin(ctx.message("script.create.creating")) do |spinner|
-              project_creator.bootstrap
+              # project_creator.bootstrap
               spinner.update_title(ctx.message("script.create.created"))
             end
           end
