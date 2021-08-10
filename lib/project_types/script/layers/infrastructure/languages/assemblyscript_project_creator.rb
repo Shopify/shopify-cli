6871c7d417320e63165bd64e623d696084b8fc13
Alex Bradley <alexandre.bradley@shopify.com>
2021-08-10 16:28:33 -0400

    add tests for refactored ProjectCreator

--- lib/project_types/script/layers/infrastructure/languages/assemblyscript_project_creator.rb
+++ lib/project_types/script/layers/infrastructure/languages/assemblyscript_project_creator.rb
@@ -6,17 +6,16 @@ module Script
       module Languages
         class AssemblyScriptProjectCreator < ProjectCreator
           MIN_NODE_VERSION = "14.5.0" # kept because task_runner uses this
+          
+          def self.config_file
+            "package.json"
+          end
 
           def setup_dependencies
             super
-            write_npmrc
-          end
-
-          def write_npmrc
             command_runner.call("npm --userconfig ./.npmrc config set @shopify:registry https://registry.npmjs.com")
             command_runner.call("npm --userconfig ./.npmrc config set engine-strict true")
           end
-
         end
       end
     end
