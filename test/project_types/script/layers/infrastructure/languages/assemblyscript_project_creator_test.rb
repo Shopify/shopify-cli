8b019bbdd682e452126c76ebcf1432ae6c81c237
Alex Bradley <alexandre.bradley@shopify.com>
2021-08-12 12:00:16 -0400

    add assemblyscript project creator tests

--- test/project_types/script/layers/infrastructure/languages/assemblyscript_project_creator_test.rb
+++ test/project_types/script/layers/infrastructure/languages/assemblyscript_project_creator_test.rb
@@ -5,16 +5,10 @@ require "project_types/script/test_helper"
 describe Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator do
   include TestHelpers::FakeFS
 
-  let(:script_name) { "myscript" }
-  let(:language) { "AssemblyScript" }
-  let(:script_id) { "id" }
   let(:context) { TestHelpers::FakeContext.new }
+  let(:fake_capture2e_response) { [nil, OpenStruct.new(success?: true)] }
+
   let(:extension_point_type) { "payment_methods" }
-  let(:extension_point) { Script::Layers::Domain::ExtensionPoint.new(extension_point_type, extension_point_config) }
-  let(:project_creator) do
-    Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
-      .new(ctx: context, extension_point: extension_point, script_name: script_name, path_to_project: script_name)
-  end
   let(:extension_point_config) do
     {
       "assemblyscript" => {
@@ -25,9 +19,49 @@ describe Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
       },
     }
   end
-  let(:fake_capture2e_response) { [nil, OpenStruct.new(success?: true)] }
+
+  let(:domain) { "fake-domain" }
+  let(:type) { extension_point_type }
+  let(:repo) { extension_point_config["assemblyscript"]["repo"] }
+  let(:script_name) { "myscript" }
+  let(:branch) { "fake-branch" }
+
+  let(:project_creator) do
+    Script::Layers::Infrastructure::Languages::AssemblyScriptProjectCreator
+      .new(
+        ctx: context,
+        domain: domain,
+        type: type,
+        repo: repo,
+        script_name: script_name,
+        path_to_project: script_name,
+        branch: branch
+      )
+  end
 
   before do
     context.mkdir_p(script_name)
+  end
+
+  describe "setup_dependencies" do
+    subject { project_creator.setup_dependencies }
+
+    it "should setup dependencies" do
+      Script::Layers::Infrastructure::Languages::ProjectCreator.any_instance
+        .expects(:setup_dependencies)
+        .with
+        .once
+
+      context
+        .expects(:capture2e)
+        .with("npm --userconfig ./.npmrc config set @shopify:registry https://registry.npmjs.com")
+        .returns(fake_capture2e_response)
+      context
+        .expects(:capture2e)
+        .with("npm --userconfig ./.npmrc config set engine-strict true")
+        .returns(fake_capture2e_response)
+
+      subject
+    end
   end
 end
