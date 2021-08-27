0b272d98a82682525a4cad6008d768e74022209a
Pedro Piñera <pepibumur@gmail.com>
2021-08-27 15:04:17 +0200

    Fix tests

--- lib/project_types/node/commands/create.rb
+++ lib/project_types/node/commands/create.rb
@@ -46,7 +46,7 @@ module Node
           scopes: "write_products,write_customers,write_draft_orders",
         ).write(@ctx)
 
-        partners_url = ShopifyCli::PartnersAPI.partners_url_for(form.organization_id, api_client["id"], local_debug?)
+        partners_url = ShopifyCli::PartnersAPI.partners_url_for(form.organization_id, api_client["id"])
 
         @ctx.puts(@ctx.message("apps.create.info.created", form.title, partners_url))
         @ctx.puts(@ctx.message("apps.create.info.serve", form.name, ShopifyCli::TOOL_NAME, "node"))
@@ -119,10 +119,6 @@ module Node
         rescue Errno::ENOENT => e
           @ctx.debug(e)
         end
-      end
-
-      def local_debug?
-        @ctx.getenv(ShopifyCli::PartnersAPI::LOCAL_DEBUG)
       end
     end
   end
