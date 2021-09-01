7c399b88975391c41056b13d27b968c033977ab8
Pedro Piñera <pepibumur@gmail.com>
2021-09-01 11:26:18 +0200

    Fix tests

--- test/shopify-cli/identity_auth_test.rb
+++ test/shopify-cli/identity_auth_test.rb
@@ -235,7 +235,7 @@ module ShopifyCli
     end
 
     def client_id
-      "e5380e02-312a-7408-5718-e07017e9cf52"
+      "fbdb2649-e327-4907-8f67-908d24cfd7e3"
     end
 
     def authorization_scopes
