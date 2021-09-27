5f53e5ba0032f87025905e50df4a31a6fedce23f
Molly Yu <molly.yu@shopify.com>
2021-09-27 10:28:37 -0400

    Fixed exit status message

--- features/support/process.rb
+++ features/support/process.rb
@@ -18,7 +18,7 @@ module Process
     args = [shopify_executable_path] + args
     cwd ||= Dir.pwd
     _, err, stat = Open3.capture3(*args, chdir: cwd)
-    raise ProcessError.new(exit_status: stat.exitstatus, stderr: err) unless stat.success?
+    raise ProcessError.new(exit_status: stat.exit_status, stderr: err) unless stat.success?
   end
 
   def self.run(*args, cwd: nil)
@@ -32,7 +32,7 @@ module Process
     cwd ||= Dir.pwd
 
     out, err, stat = Open3.capture3(*args, chdir: cwd)
-    raise ProcessError.new(exit_status: stat.exitstatus, stderr: err) unless stat.success?
+    raise ProcessError.new(exit_status: stat.exit_status, stderr: err) unless stat.success?
     out
   end
 
