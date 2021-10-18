7d358fd1153418450ea20d2cd95e2a12382d2f26
Molly Yu <molly.yu@shopify.com>
2021-10-18 14:49:16 -0400

    Working unit test

--- test/shopify-cli/github/issue_url_generator_test.rb
+++ test/shopify-cli/github/issue_url_generator_test.rb
@@ -7,17 +7,17 @@ module ShopifyCLI
         def setup
             super
             @ctx = ShopifyCLI::Context.new
-            @command = IssueURLGenerator.new(@ctx)
             @error = stub(backtrace: ["Backtrace Line 1", "Backtrace Line 2"], class: "Runtime Error", message: "Error Message")
         end
 
       def test_call_error_url
         file = File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md")
-        assert(File.exist?(file))
-        body = FIle.read(file) + "## Stacktrace\n\nTraceback:\n\n #{@error.backtrace.join("\n")}"
+        assert File.exist?(file)
+        body = File.read(file) + "## Stacktrace\n\nTraceback:\n\n #{@error.backtrace.join("\n")}"
         query = URI.encode_www_form({title: "#{@error.class}: #{@error.message}", body: body, labels: "type:bug"})
-        @ctx.expects("#{ShopifyCLI::Constants::Links::NEW_ISSUE}?#{query}")
-        @command.error_url(@error)
+        url = "#{ShopifyCLI::Constants::Links::NEW_ISSUE}?#{query}"
+        generated_url = IssueURLGenerator.error_url(@error)
+        assert_equal url, generated_url
       end
     end
   end
