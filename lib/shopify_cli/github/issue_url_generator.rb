ec3a402edad6b3a87ae0403df7f9ffa9dcd2fe62
Molly Yu <molly.yu@shopify.com>
2021-10-18 12:19:52 -0400

    Added stacktrace

--- lib/shopify_cli/github/issue_url_generator.rb
+++ lib/shopify_cli/github/issue_url_generator.rb
@@ -3,10 +3,16 @@ module ShopifyCLI
         module IssueURLGenerator
             def self.error_url(error)
                 title = "Title"
-                body = "Body"
                 labels = "type:bug"
-                template = File.read(File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md"))
-                query = URI.encode_www_form({title: title, body: template, labels: labels})
+                body = File.read(File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md"))
+                stacktrace_message = "## Stacktrace\n\nTraceback:\n\n"
+                error.backtrace[0..4].each do |e|
+                    stacktrace_message += e
+                    stacktrace_message += "\n"
+                end
+                body += stacktrace_message
+                # require 'byebug'; byebug
+                query = URI.encode_www_form({title: title, body: body, labels: labels})
                 url = "#{ShopifyCLI::Constants::Links::NEW_ISSUE}?#{query}"
                 url
               end
