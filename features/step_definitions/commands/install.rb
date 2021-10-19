7895b9af55c54ec0449570f6357c49b52f84969b
Hanna Chen <hanna.chen@shopify.com>
2021-10-19 11:20:15 -0400

    add acceptance test to check for issue template file

--- features/step_definitions/commands/install.rb
+++ features/step_definitions/commands/install.rb
@@ -1,4 +1,5 @@
 require_relative "../../../ext/shopify-extensions/shopify_extensions"
+require_relative "../../../lib/shopify_cli"
 
 When("Shopify extensions are installed in the working directory") do
   ShopifyExtensions.install(
@@ -11,4 +12,12 @@ Then("I have the right binary for my system's architecture") do
   system_architecture = [%x(uname -m).chomp].flat_map { |arch| [arch, arch.gsub("_", "-")] }
   binary_architecture = %x(file #{File.expand_path("shopify-extensions", @working_dir)})
   assert system_architecture.any? { |arch| binary_architecture.include?(arch) }
+end
+
+Given("Shopify CLI is installed on my system") do
+end
+
+Then("The file `ISSUE_TEMPLATE.md` is retained inside `.github`") do
+  issue_template_file_path = File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md")
+  assert File.exists?(issue_template_file_path)
 end
