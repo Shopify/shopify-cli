60b1f358d7bfc7378ecc342b129644f408ff6084
Molly Yu <molly.yu@shopify.com>
2021-10-25 11:55:16 -0400

    Fixed test and issue template

--- test/shopify-cli/github/issue_url_generator_test.rb
+++ test/shopify-cli/github/issue_url_generator_test.rb
@@ -6,13 +6,12 @@ module ShopifyCLI
       def setup
         super
         @ctx = ShopifyCLI::Context.new
-        @error = stub(backtrace: ["Backtrace Line 1", "Backtrace Line 2"], class: "Runtime Error",
+        @error = mock(backtrace: ["Backtrace Line 1", "Backtrace Line 2"], class: "Runtime Error",
 message: "Error Message")
       end
 
       def test_call_error_url
         file = File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md")
-        assert File.exist?(file)
         body = File.read(file) + "## Stacktrace\n\nTraceback:\n\n #{@error.backtrace.join("\n")}"
         query = URI.encode_www_form({ title: "#{@error.class}: #{@error.message}", body: body, labels: "type:bug" })
         url = "#{ShopifyCLI::Constants::Links::NEW_ISSUE}?#{query}"
