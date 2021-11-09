517390e4a70587138436faea14a79a8c13044cf6
Molly Yu <molly.yu@shopify.com>
2021-11-09 12:34:00 -0500

    Fix acceptance tests

--- features/release.feature
+++ features/release.feature
@@ -1,6 +1,7 @@
 Feature: The release process
 
   Scenario: The Ruby gem contains the ISSUE_TEMPLATE needed by some features
-    Given I have a working directory
-    When I build the Ruby Gem as shopify.gem
-    Then The shopify.gem Ruby Gem contains a file .github/ISSUE_TEMPLATE.md  
+    Given I have a VM with the CLI and a working directory
+    And I build the Ruby Gem as shopify-cli.gem
+    When I install the Ruby Gem shopify-cli.gem
+    Then The shopify-cli.gem Ruby Gem contains a file .github/ISSUE_TEMPLATE.md  
--- features/step_definitions/commands/install.rb
+++ features/step_definitions/commands/install.rb
@@ -12,29 +12,3 @@ Then("I have the right binary for my system's architecture") do
   binary_architecture = %x(file #{File.expand_path("shopify-extensions", @working_dir)})
   assert system_architecture.any? { |arch| binary_architecture.include?(arch) }
 end
-
-When(/I build the Ruby Gem as (.+)/) do |gem_name|
-  cli_root_path = Utilities::Constants::Paths::ROOT
-  gemspec_path = File.join(cli_root_path, "shopify-cli.gemspec")
-  gem_output_path = File.join(@working_dir, gem_name)
-  Process.run("gem", "build", gemspec_path, "-o", gem_output_path)
-end
-
-Then(/The (.+) Ruby Gem contains a file (.+)/) do |gem_name, file_path|
-  gem_path = File.join(@working_dir, gem_name)
-  uncompressed_gem_path = File.join(@working_dir, "shopify-cli")
-  FileUtils.mkdir_p(uncompressed_gem_path)
-  data_tar_gz_path = File.join(uncompressed_gem_path, "data.tar.gz")
-  uncompressed_data_path = File.join(@working_dir, "data")
-  FileUtils.mkdir_p(uncompressed_data_path)
-
-  Process.run("tar", "-xvzf", gem_path, "-C", uncompressed_gem_path)
-  Process.run("tar", "-xvzf", data_tar_gz_path, "-C", uncompressed_data_path)
-
-  assert File.exist?(File.join(uncompressed_data_path, file_path))
-end
-
-Then("The file `ISSUE_TEMPLATE.md` is retained inside `.github`") do
-  issue_template_file_path = File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md")
-  assert File.exist?(issue_template_file_path)
-end
