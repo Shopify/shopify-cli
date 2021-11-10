5e92c253984ff63c9babf217e32095e51089ac0a
Pedro Piñera <pepibumur@gmail.com>
2021-11-10 11:57:54 +0100

    Fix the building of the docker image

--- utilities/docker.rb
+++ utilities/docker.rb
@@ -59,7 +59,7 @@ module Utilities
         unless image_exists?(image_tag)
           puts "Rebuilding the Docker image..."
           _, err, stat = Open3.capture3(
-            "docker", "build", "-f", File.join(root_dir, "Tests.dockerfile"), "-t", image_tag
+            "docker", "build", "-t", image_tag, "-f", File.join(root_dir, "Tests.dockerfile"), root_dir
           )
           raise Error, err unless stat.success?
         end
