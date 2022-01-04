5350919572a5f5091e7c9b14d373a9ccd2eb1361
Pedro Piñera <pedro@craftweg.com>
2022-01-04 17:39:09 +0100

    Fix tests

--- test/shopify-cli/github/issue_url_generator_test.rb
+++ test/shopify-cli/github/issue_url_generator_test.rb
@@ -7,13 +7,17 @@ module ShopifyCLI
       def setup
         super
         @ctx = ShopifyCLI::Context.new
-        @error = stub(backtrace: ["Backtrace Line 1", "Backtrace Line 2"], class: "Runtime Error",
-message: "Error Message")
+        @error = nil
+        begin
+          raise StandardError, "Error"
+        rescue StandardError => error
+          @error = error
+        end
       end
 
       def test_call_error_url
         file = File.read(File.join(ShopifyCLI::ROOT, ".github/ISSUE_TEMPLATE.md"))
-        body = @error.backtrace.join("\n").to_s
+        body = @error.backtrace[0..4].join("\n").to_s
         output = file.gsub(/<!--Stacktrace(.|\n)*-->/, body)
         query = URI.encode_www_form({ title: "#{@error.class}: #{@error.message}", body: output, labels: "type:bug" })
         url = "#{ShopifyCLI::Constants::Links::NEW_ISSUE}?#{query}"
