8366180532af300ad7bfd8b05a7a57e2a1a6c474
Ariel Caplan <ariel.caplan@shopify.com>
2022-01-04 12:34:02 +0200

    Test that store= and shop= are excluded from the correction

--- test/shopify-cli/commands/command_test.rb
+++ test/shopify-cli/commands/command_test.rb
@@ -29,18 +29,32 @@ module ShopifyCLI
         assert_match(CLI::UI.fmt(ShopifyCLI::Commands::Populate::Customer.help), io.join)
       end
 
-      ["", "http://", "https://", "invalidoption=", "invalidoption=https://"].each do |prefix|
+      [
+        "",
+        "http://",
+        "https://",
+        "invalidoption=",
+        "invalidoption=https://",
+        ["store=", false],
+        ["shop=", false],
+      ].each do |prefix, correction_expected = true|
         store_name = "mystore.myshopify.com"
         store_name_with_prefix = "--#{prefix}#{store_name}"
 
         define_method("test_calls_with#{"_prefix_#{prefix}" unless prefix.empty?}_store_as_raw_param") do
           io = capture_io_and_assert_raises(ShopifyCLI::Abort) do
-            run_cmd("login #{store_name_with_prefix}")
+            run_cmd("help login #{store_name_with_prefix}")
           end
 
-          assert_message_output(io: io, expected_content: [
-            @context.message("core.errors.option_parser.invalid_option_store_equals", store_name_with_prefix, store_name),
-          ])
+          if correction_expected
+            assert_message_output(io: io, expected_content: [
+              @context.message("core.errors.option_parser.invalid_option_store_equals", store_name_with_prefix, store_name),
+            ])
+          else
+            assert_message_output(io: io, expected_content: [
+              @context.message("core.errors.option_parser.invalid_option", store_name_with_prefix),
+            ])
+          end
         end
       end
     end
