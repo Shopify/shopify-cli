a8aec50f4acad0fa19340833c594879cc2fbc27c
Alex Bradley <alexandre.bradley@shopify.com>
2022-01-28 14:57:38 -0500

    invoke taskrunner build before metadata

--- lib/project_types/script/layers/application/build_script.rb
+++ lib/project_types/script/layers/application/build_script.rb
@@ -9,12 +9,13 @@ module Script
             CLI::UI::Frame.open(ctx.message("script.application.building")) do
               begin
                 UI::StrictSpinner.spin(ctx.message("script.application.building_script")) do |spinner|
+                  script_content = task_runner.build
                   metadata_file_location = task_runner.metadata_file_location
                   metadata = Infrastructure::MetadataRepository.new(ctx: ctx).get_metadata(metadata_file_location)
 
                   Infrastructure::PushPackageRepository.new(ctx: ctx).create_push_package(
                     script_project: script_project,
-                    script_content: task_runner.build,
+                    script_content: script_content,
                     metadata: metadata,
                     library: library,
                   )
