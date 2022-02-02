f443c991d95609575aa91c59ccbef63f4285dee6
Ariel Caplan <ariel.caplan@shopify.com>
2022-02-02 15:02:37 +0200

    env vars are strings not arrays
    We were trying to `split` the resulting array, which failed!
    
    This comes up in PHP apps because the subprocess doesn't have PATHEXT
    set.

--- vendor/deps/cli-kit/lib/cli/kit/system.rb
+++ vendor/deps/cli-kit/lib/cli/kit/system.rb
@@ -227,7 +227,7 @@ module CLI
         end
 
         def which(cmd, env)
-          exts = os == :windows ? env.fetch('PATHEXT', ['']).split(';') : ['']
+          exts = (os == :windows && env['PATHEXT']&.split(';')) || ['']
           env.fetch('PATH', '').split(File::PATH_SEPARATOR).each do |path|
             exts.each do |ext|
               exe = File.join(path, "#{cmd}#{ext}")
