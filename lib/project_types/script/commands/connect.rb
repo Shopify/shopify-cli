328409388db16ed01e929b3fd8b976efa88d003d
Pedro <pedro@craftweg.com>
2022-02-03 10:44:51 +0100

    Remove unnecessary checks and update the CHANGELOG

--- lib/project_types/script/commands/connect.rb
+++ lib/project_types/script/commands/connect.rb
@@ -5,10 +5,6 @@ module Script
       prerequisite_task :ensure_authenticated
       prerequisite_task ensure_project_type: :script
 
-      recommend_node(
-        from: ::Script::Layers::Infrastructure::Languages::TypeScriptProjectCreator::MIN_NODE_VERSION,
-        to: ShopifyCLI::Constants::SupportedVersions::Node::TO
-      )
       recommend_default_ruby_range
 
       def call(_args, _)
