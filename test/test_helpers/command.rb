8a84dc35424e82ecb8062d0456ef4cf92efd4155
Pedro <pedro@craftweg.com>
2022-02-09 13:29:51 +0100

    Update the CHANGELOG

--- test/test_helpers/command.rb
+++ test/test_helpers/command.rb
@@ -2,7 +2,7 @@ module TestHelpers
   module Command
     def setup
       ShopifyCLI::Command.stubs(:check_node_version)
-      ShopifyCLI::Command.stubs(:check_node_version)
+      ShopifyCLI::Command.stubs(:check_ruby_version)
       super
     end
   end
