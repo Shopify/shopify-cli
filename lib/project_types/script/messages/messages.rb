52ae0f0c3844f0caed7eefb8b55d572aa06990a2
Alex Bradley <alexandre.bradley@shopify.com>
2022-02-14 12:02:12 -0500

    Update lib/project_types/script/messages/messages.rb
    Co-authored-by: Jacob Steves <jacob.steves@shopify.com>

--- lib/project_types/script/messages/messages.rb
+++ lib/project_types/script/messages/messages.rb
@@ -114,7 +114,7 @@ module Script
           dependency_install_help: "Correct the errors.",
 
           invalid_environment_cause: "Your environment %{tool} version, %{env_version}, "\
-                                     "is too low. It must be greater than %{minimum_version}.",
+                                     "is too low. It must be at least %{minimum_version}.",
           invalid_environment_help: "Update %{tool}.",
 
           failed_api_request_cause: "Something went wrong while communicating with Shopify.",
