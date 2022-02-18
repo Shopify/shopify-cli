a3c5682e36ca5b1d58e51739cd9594d30da9c688
kilgore5 <john@kingandpartners.com>
2022-02-18 01:55:10 -0500

    rubocop

--- lib/project_types/theme/commands/pull.rb
+++ lib/project_types/theme/commands/pull.rb
@@ -75,7 +75,7 @@ module Theme
 
         if development
           dev_theme = ShopifyCLI::Theme::DevelopmentTheme.find(@ctx, root: root)
-          return dev_theme || @ctx.abort(@ctx.message("theme.pull.theme_not_found", 'development'))
+          return dev_theme || @ctx.abort(@ctx.message("theme.pull.theme_not_found", "development"))
         end
 
         select_theme(root)
