81abd0c412b5214b649a166ac1501d9b3836496e
Guilherme Carreiro <karreiro@gmail.com>
2022-03-17 09:20:32 +0100

    Fix `ThemeAdminAPI` not to handle asset errors (#2149)

--- lib/shopify_cli/theme/theme_admin_api.rb
+++ lib/shopify_cli/theme/theme_admin_api.rb
@@ -11,35 +11,19 @@ module ShopifyCLI
       end
 
       def get(path:, **args)
-        rest_request(
-          method: "GET",
-          path: path,
-          **args
-        )
+        rest_request(method: "GET", path: path, **args)
       end
 
       def put(path:, **args)
-        rest_request(
-          method: "PUT",
-          path: path,
-          **args
-        )
+        rest_request(method: "PUT", path: path, **args)
       end
 
       def post(path:, **args)
-        rest_request(
-          method: "POST",
-          path: path,
-          **args
-        )
+        rest_request(method: "POST", path: path, **args)
       end
 
       def delete(path:, **args)
-        rest_request(
-          method: "DELETE",
-          path: path,
-          **args
-        )
+        rest_request(method: "DELETE", path: path, **args)
       end
 
       def get_shop_or_abort # rubocop:disable Naming/AccessorMethodName
@@ -56,15 +40,32 @@ module ShopifyCLI
           **args.compact
         )
       rescue ShopifyCLI::API::APIRequestForbiddenError,
-             ShopifyCLI::API::APIRequestUnauthorizedError
-        handle_permissions_error
+             ShopifyCLI::API::APIRequestUnauthorizedError => error
+        # The Admin API returns 403 Forbidden responses on different
+        # scenarios:
+        #
+        # * when a user doesn't have permissions for a request:
+        #   <APIRequestForbiddenError: 403 {}>
+        #
+        # * when an asset operation cannot be performed:
+        #   <APIRequestForbiddenError: 403 {"message":"templates/gift_card.liquid could not be deleted"}>
+        if empty_response_error?(error)
+          return handle_permissions_error
+        end
+
+        raise error
       end
 
       def handle_permissions_error
-        ensure_user_error = @ctx.message("theme.ensure_user_error", get_shop_or_abort)
+        ensure_user_error = @ctx.message("theme.ensure_user_error", shop)
         ensure_user_try_this = @ctx.message("theme.ensure_user_try_this")
 
         @ctx.abort(ensure_user_error, ensure_user_try_this)
+      end
+
+      def empty_response_error?(error)
+        error_message = error&.response&.body.to_s
+        error_message.empty?
       end
     end
   end
