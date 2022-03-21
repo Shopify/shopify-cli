7b261ce2b9dd845dcf5876d2fd941480602d7bf5
David Henry <david.henry@shopify.com>
2022-03-21 15:36:46 -0400

    Re-add test file

--- lib/project_types/extension/forms/questions/ask_template.rb
+++ lib/project_types/extension/forms/questions/ask_template.rb
@@ -11,14 +11,22 @@ module Extension
           default: -> { CLI::UI::Prompt.method(:ask) }
 
         def call(project_details)
-          type = project_details&.type&.identifier
-          if Models::DevelopmentServerRequirements.supported?(type)
-            project_details.template = template || choose_interactively
-          end
+          return project_details unless template_required?(project_details)
+          project_details.template = template || choose_interactively
           project_details
         end
 
         private
+
+        def template_required?(project_details)
+          return false unless extension_server_beta?
+          type = project_details&.type&.identifier
+          Models::DevelopmentServerRequirements::SUPPORTED_EXTENSION_TYPES.include?(type.downcase)
+        end
+
+        def extension_server_beta?
+          ShopifyCLI::Feature.enabled?(:extension_server_beta)
+        end
 
         def choose_interactively
           prompt.call(ctx.message("create.ask_template")) do |handler|
