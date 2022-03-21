77057f339ffa9bc6aafa44b5b8d1eaef75b397c3
Guilherme Carreiro <karreiro@gmail.com>
2022-03-21 16:19:48 +0100

    Fix `BaseGlob` coverter to support multiple flags (#2122)

--- lib/project_types/theme/commands/pull.rb
+++ lib/project_types/theme/commands/pull.rb
@@ -26,11 +26,11 @@ module Theme
         parser.on("-d", "--development") { flags[:development] = true }
         parser.on("-o", "--only=PATTERN", Conversions::IncludeGlob) do |pattern|
           flags[:includes] ||= []
-          flags[:includes] += pattern
+          flags[:includes] |= pattern
         end
         parser.on("-x", "--ignore=PATTERN", Conversions::IgnoreGlob) do |pattern|
           flags[:ignores] ||= []
-          flags[:ignores] += pattern
+          flags[:ignores] |= pattern
         end
       end
 
