f1b441f297b023ecc90fa8ca358bdade31e6cd64
Gonzalo Riestra <gonzaloriestra@gmail.com>
2022-03-29 11:32:50 +0200

    Avoid updating the URL when the flag is passed

--- lib/shopify_cli/services/app/serve/rails_service.rb
+++ lib/shopify_cli/services/app/serve/rails_service.rb
@@ -2,32 +2,9 @@ module ShopifyCLI
   module Services
     module App
       module Serve
-        class RailsService < BaseService
-          attr_accessor :host, :port, :context
-
-          def initialize(host:, port:, context:)
-            @host = host
-            @port = port
-            @context = context
-            super()
-          end
-
+        class RailsService < ServeService
           def call
-            project = ShopifyCLI::Project.current
-            url = host || ShopifyCLI::Tunnel.start(context, port: port)
-            raise ShopifyCLI::Abort,
-              context.message("core.app.serve.error.host_must_be_https") if url.match(/^https/i).nil?
-            project.env.update(context, :host, url)
-            ShopifyCLI::Tasks::UpdateDashboardURLS.call(
-              context,
-              url: url,
-              callback_url: "/auth/shopify/callback",
-            )
-
-            if project.env.shop
-              project_url = "#{project.env.host}/login?shop=#{project.env.shop}"
-              context.puts("\n" + context.message("core.app.serve.open_info", project_url) + "\n")
-            end
+            generate_url
 
             CLI::UI::Frame.open(context.message("core.app.serve.running_server")) do
               env = ShopifyCLI::Project.current.env.to_h
