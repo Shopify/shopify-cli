77910b1acde221adb17a63736d79dff8a908b947
Jesse Tuğlu <jesse.tuglu@shopify.com>
2022-06-28 12:10:57 -0400

    abstract watcher.rb logic to FileSystemListener.rb interface (#2427)

---
+++ lib/shopify_cli/file_system_listener.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+require "listen"
+require "observer"
+
+module ShopifyCLI
+  class FileSystemListener
+    include Observable
+
+    def initialize(root:, force_poll:, ignore_regex:)
+      @root = root
+      @force_poll = force_poll
+      @ignore_regex = ignore_regex
+
+      @listener = Listen.to(@root, force_polling: @force_poll, ignore: @ignore_regex) do |updated, added, removed|
+        changed
+        notify_observers(updated, added, removed)
+      end
+    end
+
+    def start
+      @listener.start
+    end
+
+    def stop
+      @listener.stop
+    end
+  end
+end
