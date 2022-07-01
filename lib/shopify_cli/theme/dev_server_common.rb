597d09ef66a99c407cd58a1cd132d98d779647ad
Morisa Manzella <morisa.manzella@shopify.com>
2022-07-01 13:08:30 -0400

    add dev server common class (not a huge fan of this name)

---
+++ lib/shopify_cli/theme/dev_server_common.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require "pathname"
+
+module ShopifyCLI
+  module Theme
+    module DevServer
+      # Errors
+      Error = Class.new(StandardError)
+      AddressBindingError = Class.new(Error)
+
+      class DevServerCommon
+        class << self
+          attr_accessor :ctx
+        end
+      end
+    end
+  end 
+end 
