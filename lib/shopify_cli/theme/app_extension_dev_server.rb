5f9ace0c4d2903bd99c671c9b13723bfbc87219c
Morisa Manzella <morisa.manzella@shopify.com>
2022-07-01 11:01:36 -0400

    remove old web server class

--- lib/shopify_cli/theme/app_extension_dev_server.rb
+++ lib/shopify_cli/theme/app_extension_dev_server.rb
@@ -12,28 +12,7 @@ module ShopifyCLI
       # Errors
       Error = Class.new(StandardError)
       AddressBindingError = Class.new(Error)
-
-      # # move logic from web server to lib/shopify_cli/thread_pool.rb
-      # class AppExtensionWebServer < WEBrick::HTTPServlet::AbstractServlet
-      #   def self.run
-      #     options = {
-      #       :BindAddress=>"127.0.0.1", 
-      #       :Port=>9292
-      #     }
-      #     @server = ::WEBrick::HTTPServer.new(options)
-      #     @server.mount '/', AppExtensionWebServer
-      #     @server.start
-      #   end
-
-      #   def do_GET request, response
-      #     # status, content_type, body = do_stuff_with request
-
-      #     response.status = 200
-      #     response['Content-Type'] = 'text/plain'
-      #     response.body = 'Hello, World!'
-      #   end
-      # end
-
+      
       class << self
         attr_accessor :ctx
 
