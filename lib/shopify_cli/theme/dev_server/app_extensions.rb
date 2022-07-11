cadb4f34878000ce34056a612e622cd8beba2765
Morisa Manzella <morisa.manzella@shopify.com>
2022-07-11 07:37:56 -0400

    Create dev server for theme app extensions (#2429)

---
+++ lib/shopify_cli/theme/dev_server/app_extensions.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module ShopifyCLI
+  module Theme
+    module DevServer
+      class AppExtensions
+        def call(_env)
+          headers = {}
+
+          headers["Content-Type"] = "text/plain"
+          [200, headers, ["Hello, World!"]]
+        end
+
+        def close
+          @streams.close
+        end
+      end
+    end
+  end
+end
