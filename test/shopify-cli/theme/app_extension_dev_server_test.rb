e24d4adfb62baaf1306d5fcbe6f43665ff05fcda
Jesse Tuglu <jesse.tuglu@shopify.com>
2022-07-12 11:04:28 -0400

    PR changes

--- test/shopify-cli/theme/app_extension_dev_server_test.rb
+++ test/shopify-cli/theme/app_extension_dev_server_test.rb
@@ -8,10 +8,27 @@ module ShopifyCLI
       def setup
         super
         @ctx = ShopifyCLI::Context.new
+        @theme = stub(
+          "Host Theme Testing",
+          root: ".",
+          id: 1234,
+          name: "HostTheme Test",
+          shop: "test.myshopify.io",
+          editor_url: "https://test.myshopify.io/editor",
+          preview_url: "https://test.myshopify.io/preview",
+          live?: false,
+        )
+        @extension = stub(
+          "Theme App Extension",
+          root: ".",
+          id: 1234,
+          name: "Theme App Extension",
+        )
         ShopifyCLI::Theme::DevServer::AppExtensionDevServer.ctx = @ctx
       end
 
       def test_start_calls_run_webserver
+        skip # TODO: once HostTheme abstraction is created modify tests
         logger = mock("Logger")
         app = mock("App")
 
@@ -27,10 +44,16 @@ module ShopifyCLI
           .with(app, BindAddress: "127.0.0.1", Port: 9292, Logger: logger, AccessLog: [])
           .returns(nil)
 
+        ShopifyCLI::Theme::Theme
+          .expects(:find_by_identifier)
+          .with(@ctx, root: @theme.root + "/../tmp_theme", identifier: @theme.name)
+          .returns(@theme)
+
         simulate_start_server
       end
 
       def test_stop_calls_shutdown_webserver
+        skip # TODO: once HostTheme abstraction is created modify tests
         ShopifyCLI::Theme::DevServer::WebServer
           .expects(:shutdown)
           .returns(nil)
