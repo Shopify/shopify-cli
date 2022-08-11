cd30a8877a1e6b4350675f76b980f7948026f37e
Ariel Caplan <ariel.caplan@shopify.com>
2022-08-11 12:01:21 +0300

    Naming fixes

--- lib/shopify_cli/core/cli_version.rb
+++ lib/shopify_cli/core/cli_version.rb
@@ -1,17 +1,17 @@
 module ShopifyCLI
   module Core
     ##
-    # ShopifyCLI::CLI checks that the CLI in use is correct for the project.
+    # ShopifyCLI::Core::CliVersion checks that the CLI in use is correct for the project.
     #
     class CliVersion
       class << self
         def using_3_0?
-          !!cli_3_0_toml
+          !!cli_3_0_toml_dir
         end
 
         private
 
-        def cli_3_0_toml
+        def cli_3_0_toml_dir
           Utilities.directory("shopify.app.toml", Dir.pwd)
         end
       end
