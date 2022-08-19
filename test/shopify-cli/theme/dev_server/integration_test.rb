052301fed30ff1c27a1af72620df587c6d40f4b3
Jesse Tuglu <jesse.tuglu@shopify.com>
2022-08-19 16:01:36 -0400

    test

--- test/shopify-cli/theme/dev_server/integration_test.rb
+++ test/shopify-cli/theme/dev_server/integration_test.rb
@@ -8,15 +8,11 @@ module ShopifyCLI
       class IntegrationTest < Minitest::Test
         include TestHelpers::FakeUI
 
-        @@port = 9292 # rubocop:disable Style/ClassVars
-
         THEMES_API_URL = "https://dev-theme-server-store.myshopify.com/admin/api/unstable/themes/123456789.json"
         ASSETS_API_URL = "https://dev-theme-server-store.myshopify.com/admin/api/unstable/themes/123456789/assets.json"
 
         def setup
           super
-
-          WebMock.disable_net_connect!(allow: "127.0.0.1:#{@@port}")
 
           ShopifyCLI::DB.expects(:exists?).with(:shop).at_least_once.returns(true)
           ShopifyCLI::DB.expects(:get).with(:shop).at_least_once.returns("dev-theme-server-store.myshopify.com")
@@ -36,11 +32,14 @@ module ShopifyCLI
             TestHelpers::Singleton.reset_singleton!(DevServer.instance)
             @server_thread.join
           end
-          @@port += 1 # rubocop:disable Style/ClassVars
         end
 
         def test_proxy_to_sfr
-          skip("Causing flaky behavior in CI, need to revisit")
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9292
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+
           stub_request(:any, ASSETS_API_URL)
             .to_return(status: 200, body: "{}")
           stub_request(:any, THEMES_API_URL)
@@ -54,16 +53,19 @@ module ShopifyCLI
             )
           stub_sfr = stub_request(:get, "https://dev-theme-server-store.myshopify.com/?_fd=0&pb=0")
 
-          start_server
-          response = get("/")
+          start_server(port)
+          response = get(port,"/")
 
           refute_server_errors(response)
           assert_requested(stub_sfr)
         end
 
         def test_uploads_files_on_boot
-          skip("Causing flaky behavior in CI, need to revisit")
-          start_server_and_wait_sync_files
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9293
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+          start_server_and_wait_sync_files(port)
 
           # Should upload all theme files except the ignored files
           ignored_files = %w[config.yml super_secret.json settings_data.json ignores_file]
@@ -85,8 +87,11 @@ module ShopifyCLI
         end
 
         def test_uploads_files_on_modification
-          skip("Causing flaky behavior in CI, need to revisit")
-          start_server_and_wait_sync_files
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9294
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+          start_server_and_wait_sync_files(port)
 
           theme_root = "#{ShopifyCLI::ROOT}/test/fixtures/theme"
 
@@ -110,37 +115,46 @@ module ShopifyCLI
         end
 
         def test_serve_assets_locally
-          skip("Causing flaky behavior in CI, need to revisit")
-          response = start_server_and_wait_sync_files
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9295
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+          response = start_server_and_wait_sync_files(port)
 
           refute_server_errors(response)
         end
 
         def test_address_already_in_use
-          skip("Causing flaky behavior in CI, need to revisit")
-          start_server_and_wait_sync_files
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9296
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+          start_server_and_wait_sync_files(port)
 
           # Stub StandardReporter#report to keep test logs clean
           ShopifyCLI::Theme::Syncer::StandardReporter.any_instance.stubs(:report)
 
           @ctx.output_captured = true
           io = capture_io_and_assert_raises(ShopifyCLI::AbortSilent) do
-            DevServer.start(@ctx, "#{ShopifyCLI::ROOT}/test/fixtures/theme", port: @@port, stable: true)
+            DevServer.start(@ctx, "#{ShopifyCLI::ROOT}/test/fixtures/theme", port: port, stable: true)
           end
           @ctx.output_captured = false
 
           io_messages = io.join
 
-          assert_match(@ctx.message("theme.serve.address_already_in_use", "http://127.0.0.1:#{@@port}"), io_messages)
+          assert_match(@ctx.message("theme.serve.address_already_in_use", "http://127.0.0.1:#{port}"), io_messages)
           assert_match(@ctx.message("theme.serve.try_port_option"), io_messages)
         end
 
         def test_streams_hot_reload_events
-          skip("Causing flaky behavior in CI, need to revisit")
-          start_server_and_wait_sync_files
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9297
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+          start_server_and_wait_sync_files(port)
 
           # Send the SSE request
-          socket = TCPSocket.new("127.0.0.1", @@port)
+          socket = TCPSocket.new("127.0.0.1", port)
           socket.write("GET /hot-reload HTTP/1.1\r\n")
           socket.write("Host: 127.0.0.1\r\n")
           socket.write("\r\n")
@@ -159,7 +173,11 @@ module ShopifyCLI
         end
 
         def test_forbidden_error
-          skip("Causing flaky behavior in CI, need to revisit")
+          puts __method__
+          # skip("Causing flaky behavior in CI, need to revisit")
+          port = 9298
+          WebMock.disable_net_connect!(allow: "127.0.0.1:#{port}")
+
           root = "#{ShopifyCLI::ROOT}/test/fixtures/theme"
           ctx = TestHelpers::FakeContext.new(root: root)
           error_message = "error message"
@@ -171,16 +189,16 @@ module ShopifyCLI
           ctx.expects(:abort).with(error_message)
 
           DevelopmentTheme.stubs(:find_or_create!).raises(ShopifyCLI::API::APIRequestForbiddenError)
-          DevServer.start(ctx, "#{ShopifyCLI::ROOT}/test/fixtures/theme", port: @@port, stable: true)
+          DevServer.start(ctx, "#{ShopifyCLI::ROOT}/test/fixtures/theme", port: port, stable: true)
         end
 
         private
 
-        def start_server
+        def start_server(port)
           @ctx = TestHelpers::FakeContext.new(root: "#{ShopifyCLI::ROOT}/test/fixtures/theme")
 
           @server_thread = Thread.new do
-            DevServer.start(@ctx, "#{ShopifyCLI::ROOT}/test/fixtures/theme", port: @@port, stable: true)
+            DevServer.start(@ctx, "#{ShopifyCLI::ROOT}/test/fixtures/theme", port: port, stable: true)
           rescue => e
             puts "Failed to start DevServer:"
             puts e.message
@@ -188,7 +206,7 @@ module ShopifyCLI
           end
         end
 
-        def start_server_and_wait_sync_files
+        def start_server_and_wait_sync_files(port)
           # Get the checksums
           stub_request(:any, ASSETS_API_URL)
             .to_return(status: 200, body: "{}")
@@ -198,18 +216,18 @@ module ShopifyCLI
           stub_request(:get, "#{ASSETS_API_URL}?asset%5Bkey%5D=assets/added.css")
             .to_return(status: 200, body: "{}")
 
-          start_server
+          start_server(port)
           # Wait for server to start & sync the files
-          get("/assets/bogus.css")
+          get(port, "/assets/bogus.css")
         end
 
         def refute_server_errors(response)
           refute_match(/error/i, response, response)
         end
 
-        def get(path)
+        def get(port, path)
           with_retries(Errno::ECONNREFUSED) do
-            Net::HTTP.get(URI("http://127.0.0.1:#{@@port}#{path}"))
+            Net::HTTP.get(URI("http://127.0.0.1:#{port}#{path}"))
           end
         end
 
