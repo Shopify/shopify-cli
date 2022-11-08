5017859057722dc273d7a28960da2f364cd81e25
Julien Poitrin <julien.poitrin@shopify.com>
2022-11-08 14:42:27 +0100

    Ignore superfluous folders

--- lib/project_types/theme/models/specification_handlers/theme.rb
+++ lib/project_types/theme/models/specification_handlers/theme.rb
@@ -5,11 +5,9 @@ module Theme
     module SpecificationHandlers
       class Theme
         REQUIRED_FOLDERS = %w(config layout sections templates templates/customers).map { |folder| "#{folder}/" }
-        OPTIONAL_FOLDERS = %w(assets locales snippets).map { |folder| "#{folder}/" }
 
         attr_accessor :folders
         attr_accessor :missing_folders
-        attr_accessor :superfluous_folders
 
         def initialize(root)
           return if root.nil?
@@ -21,13 +19,12 @@ module Theme
 
         def valid?
           self.validate
-          self.superfluous_folders.empty? && self.missing_folders.empty?
+          self.missing_folders.empty?
         end
 
         private
 
         def validate
-          self.superfluous_folders = self.folders - REQUIRED_FOLDERS - OPTIONAL_FOLDERS
           self.missing_folders = REQUIRED_FOLDERS - self.folders
         end
       end
--- test/project_types/theme/models/specification_handlers/theme_test.rb
+++ test/project_types/theme/models/specification_handlers/theme_test.rb
@@ -13,22 +13,21 @@ module Theme
         end
 
         def test_valid_directory_structure
-          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS + Theme::OPTIONAL_FOLDERS
+          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS
           assert @theme_specification_handler.valid?
         end
 
-        def test_required_folders_missing
+        def test_missing_folders
           @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS.first(2)
           refute @theme_specification_handler.valid?
           assert_equal Theme::REQUIRED_FOLDERS.last(3), @theme_specification_handler.missing_folders
         end
 
-        def test_superfluous_folders_present
-          superfluous_folders = ["foo/", "bar/"]
-          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS + superfluous_folders
-          refute @theme_specification_handler.valid?
+        def test_additional_folders
+          additional_folders = ["webpack/", "foobar/"]
+          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS + additional_folders
+          assert @theme_specification_handler.valid?
           assert_empty @theme_specification_handler.missing_folders
-          assert_equal superfluous_folders, @theme_specification_handler.superfluous_folders
         end
       end
     end
