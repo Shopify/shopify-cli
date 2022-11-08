312daacc80068c1a6aa95eb63bd62dfd0bef93c7
Julien Poitrin <julien.poitrin@shopify.com>
2022-11-08 14:17:48 +0100

    Get rid of root and templates folders differentiation

--- lib/project_types/theme/models/specification_handlers/theme.rb
+++ lib/project_types/theme/models/specification_handlers/theme.rb
@@ -4,36 +4,31 @@ module Theme
   module Models
     module SpecificationHandlers
       class Theme
-        REQUIRED_ROOT_FOLDERS = %w(config layout sections templates).map { |folder| "#{folder}/" }
-        OPTIONAL_ROOT_FOLDERS = %w(assets locales snippets).map { |folder| "#{folder}/" }
-        REQUIRED_TEMPLATES_FOLDERS = %w(customers).map { |folder| "templates/#{folder}/" }
+        REQUIRED_FOLDERS = %w(config layout sections templates templates/customers).map { |folder| "#{folder}/" }
+        OPTIONAL_FOLDERS = %w(assets locales snippets).map { |folder| "#{folder}/" }
 
-        attr_accessor :root_folders
-        attr_accessor :templates_folders
-        attr_accessor :superfluous_root_folders
-        attr_accessor :missing_root_folders
-        attr_accessor :superfluous_templates_folders
-        attr_accessor :missing_templates_folders
+        attr_accessor :folders
+        attr_accessor :missing_folders
+        attr_accessor :superfluous_folders
 
         def initialize(root)
           return if root.nil?
 
           Dir.chdir(root) do
-            self.root_folders, self.templates_folders = Dir["*/"], Dir["templates/*/"]
+            self.folders = Dir["*/"] + Dir["templates/*/"]
           end
         end
 
-        def validate
-          self.superfluous_root_folders = self.root_folders - REQUIRED_ROOT_FOLDERS - OPTIONAL_ROOT_FOLDERS
-          self.missing_root_folders = REQUIRED_ROOT_FOLDERS - self.root_folders
-
-          self.superfluous_templates_folders = self.templates_folders - REQUIRED_TEMPLATES_FOLDERS
-          self.missing_templates_folders = REQUIRED_TEMPLATES_FOLDERS - self.templates_folders
+        def valid?
+          self.validate
+          self.superfluous_folders.empty? && self.missing_folders.empty?
         end
 
-        def valid?
-          self.superfluous_root_folders.empty? && self.missing_root_folders.empty? &&
-          self.superfluous_templates_folders.empty? && self.missing_templates_folders.empty?
+        private
+
+        def validate
+          self.superfluous_folders = self.folders - REQUIRED_FOLDERS - OPTIONAL_FOLDERS
+          self.missing_folders = REQUIRED_FOLDERS - self.folders
         end
       end
     end
--- test/project_types/theme/models/specification_handlers/theme_test.rb
+++ test/project_types/theme/models/specification_handlers/theme_test.rb
@@ -13,27 +13,22 @@ module Theme
         end
 
         def test_valid_directory_structure
-          @theme_specification_handler.root_folders = Theme::REQUIRED_ROOT_FOLDERS + Theme::OPTIONAL_ROOT_FOLDERS
-          @theme_specification_handler.templates_folders = Theme::REQUIRED_TEMPLATES_FOLDERS
-          @theme_specification_handler.validate
+          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS + Theme::OPTIONAL_FOLDERS
           assert @theme_specification_handler.valid?
         end
 
         def test_required_folders_missing
-          @theme_specification_handler.root_folders = Theme::REQUIRED_ROOT_FOLDERS.first(2)
-          @theme_specification_handler.templates_folders = []
-          @theme_specification_handler.validate
+          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS.first(2)
           refute @theme_specification_handler.valid?
-          assert_equal Theme::REQUIRED_ROOT_FOLDERS.last(2), @theme_specification_handler.missing_root_folders
+          assert_equal Theme::REQUIRED_FOLDERS.last(3), @theme_specification_handler.missing_folders
         end
 
         def test_superfluous_folders_present
           superfluous_folders = ["foo/", "bar/"]
-          @theme_specification_handler.root_folders = Theme::REQUIRED_ROOT_FOLDERS + superfluous_folders
-          @theme_specification_handler.templates_folders = Theme::REQUIRED_TEMPLATES_FOLDERS + superfluous_folders
-          @theme_specification_handler.validate
-          assert_empty @theme_specification_handler.missing_root_folders
-          assert_equal superfluous_folders, @theme_specification_handler.superfluous_root_folders
+          @theme_specification_handler.folders = Theme::REQUIRED_FOLDERS + superfluous_folders
+          refute @theme_specification_handler.valid?
+          assert_empty @theme_specification_handler.missing_folders
+          assert_equal superfluous_folders, @theme_specification_handler.superfluous_folders
         end
       end
     end
