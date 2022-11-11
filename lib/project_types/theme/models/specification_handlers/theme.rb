c5a6c212d85b2dcfdf808b15b4607fddea5dcfe4
Julien Poitrin <julien.poitrin@shopify.com>
2022-11-11 10:59:34 +0100

    Use @ root, instead of attr_accessor

--- lib/project_types/theme/models/specification_handlers/theme.rb
+++ lib/project_types/theme/models/specification_handlers/theme.rb
@@ -7,16 +7,12 @@ module Theme
         REQUIRED_FOLDERS = %w(config layout sections templates)
 
         def initialize(root)
-          self.root = root
+          @root = root
         end
 
         def valid?
-          REQUIRED_FOLDERS.all? { |required_folder| Dir.exist?(File.join(root, required_folder)) }
+          REQUIRED_FOLDERS.all? { |required_folder| Dir.exist?(File.join(@root, required_folder)) }
         end
-
-        private
-
-        attr_accessor :root
       end
     end
   end
